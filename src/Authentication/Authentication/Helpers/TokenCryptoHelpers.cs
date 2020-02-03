dcbf5fbf5f477c7a081828907569a2857ea6a5bf
Peter Ombwa <peombwa@microsoft.com>
2020-02-03 11:17:45 -0800

    Pre load System.Security.Cryptography.ProtectedData assembly on Windows PowerShell.

---
+++ src/Authentication/Authentication/Helpers/TokenCryptoHelpers.cs
@@ -0,0 +1,38 @@
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+namespace Microsoft.Graph.PowerShell.Authentication.Helpers
+{
+    using System;
+    using System.Security.Cryptography;
+
+    /// <summary>
+    /// Helper class to handle token encryption and decryption.
+    /// </summary>
+    internal static class TokenCryptoHelpers
+    {
+        /// <summary>
+        /// Encrypts the passed buffer based on the host platform.
+        /// </summary>
+        /// <param name="buffer">A <see cref="byte[]"/> to encrypt.</param>
+        /// <returns>An encrypted <see cref="byte[]"/>.</returns>
+        public static byte[] EncryptToken(byte[] buffer)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                return ProtectedData.Protect(buffer, null, DataProtectionScope.CurrentUser);
+            return buffer;
+        }
+
+        /// <summary>
+        /// Decrypts the passed buffer based on the host platform.
+        /// </summary>
+        /// <param name="buffer">A <see cref="byte[]"/> to decrypt.</param>
+        /// <returns>An decrypted <see cref="byte[]"/>.</returns>
+        public static byte[] DecryptToken(byte[] buffer)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                return ProtectedData.Unprotect(buffer, null, DataProtectionScope.CurrentUser);
+            return buffer;
+        }
+    }
+}
