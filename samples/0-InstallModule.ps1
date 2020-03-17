8ef45facc5260a5bf6905a27a40598249f845e4d
Peter Ombwa <peter.ombwa@microsoft.com>
2020-03-16 17:52:34 -0700

    Update 0-InstallModule.ps1

--- samples/0-InstallModule.ps1
+++ samples/0-InstallModule.ps1
@@ -9,8 +9,11 @@ Install-module Microsoft.Graph
 Uninstall-Module Microsoft.Graph
 
 # Remove all the dependent modules
-Get-InstalledModule Microsoft.Graph.* | uninstall-module
+Get-InstalledModule Microsoft.Graph.* | %{ if($_.Name -ne "Microsoft.Graph.Authentication"){ Uninstall-Module $_.Name } }
 
-# Update the authentication module from 0.1.4 to 0.1.5
+# Update the authentication module from 0.1.5 to 0.2.0
 Install-Module Microsoft.Graph.Authentication -Repository PSGallery -force
+
+# Or uninstall the authentication module
+Uninstall-Module Microsoft.Graph.Authentication
 
