cdd4ddd2a696d477fe37a139fe435f0aef3dc308
Peter Ombwa <peombwa@microsoft.com>
2020-05-06 16:28:32 -0700

    Detect OS platform

--- src/Authentication/Authentication/Helpers/PlatformHelpers.cs
+++ src/Authentication/Authentication/Helpers/PlatformHelpers.cs
@@ -5,25 +5,18 @@
 namespace Microsoft.Graph.PowerShell.Authentication.Helpers
 {
     using System;
-    public static class PlatformHelpers
+    using System.Runtime.InteropServices;
+
+    public static class OperatingSystem
     {
         /// <summary>
         /// Detects if the platform we are running on is Windows or not.
         /// </summary>
         /// <returns>True if we are runnning on Windows or False if we are not.</returns>
-        public static bool IsWindows()
-        {
-            return Environment.OSVersion.Platform == PlatformID.Win32NT;
-        }
+        public static bool IsWindows() => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
-        public static bool IsMacOS()
-        {
-            return Environment.OSVersion.Platform == PlatformID.Unix;
-        }
+        public static bool IsMacOS() => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
 
-        public static bool IsLinux()
-        {
-            return Environment.OSVersion.Platform == PlatformID.Unix;
-        }
+        public static bool IsLinux() => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
     }
 }
--- src/Authentication/Authentication/TokenCache/TokenCryptographer.cs
+++ src/Authentication/Authentication/TokenCache/TokenCryptographer.cs
@@ -19,9 +19,9 @@ namespace Microsoft.Graph.PowerShell.Authentication.TokenCache
         /// <param name="accessToken">An access token.</param>
         public static void EncryptAndSetToken(string appId, string tokenCacheFilePath, byte[] accessToken)
         {
-            if (PlatformHelpers.IsWindows())
+            if (Helpers.OperatingSystem.IsWindows())
                 WindowsTokenCache.SetToken(tokenCacheFilePath, accessToken);
-            else if (PlatformHelpers.IsMacOS())
+            else if (Helpers.OperatingSystem.IsMacOS())
                 MacTokenCache.SetToken(appId, accessToken);
             else
                 LinuxTokenCache.SetToken(appId, accessToken);
@@ -35,9 +35,9 @@ namespace Microsoft.Graph.PowerShell.Authentication.TokenCache
         /// <returns></returns>
         public static byte[] DencryptAndGetToken(string appId, string tokenCacheFilePath)
         {
-            if (PlatformHelpers.IsWindows())
+            if (Helpers.OperatingSystem.IsWindows())
                 return WindowsTokenCache.GetToken(tokenCacheFilePath);
-            else if (PlatformHelpers.IsMacOS())
+            else if (Helpers.OperatingSystem.IsMacOS())
                 return MacTokenCache.GetToken(appId);
             else
                 return LinuxTokenCache.GetToken(appId);
@@ -50,9 +50,9 @@ namespace Microsoft.Graph.PowerShell.Authentication.TokenCache
         /// <param name="tokenCacheFilePath">Path to the token cache file.</param>
         public static void DeleteTokenFromCache(string appId, string tokenCacheFilePath)
         {
-            if (PlatformHelpers.IsWindows())
+            if (Helpers.OperatingSystem.IsWindows())
                 WindowsTokenCache.DeleteToken(tokenCacheFilePath);
-            else if (PlatformHelpers.IsMacOS())
+            else if (Helpers.OperatingSystem.IsMacOS())
                 MacTokenCache.DeleteToken(appId);
             else
                 LinuxTokenCache.DeleteToken(appId);
