4db6d4a2e11b249d18fcabe7e6f521532db46da1
Peter Ombwa <peombwa@microsoft.com>
2020-08-03 17:28:32 -0700

    Add sample to mention a user on Teams.

--- samples/5-Chats.ps1
+++ samples/5-Chats.ps1
@@ -8,3 +8,16 @@ Get-MgChatMessage -chatId $chatId
 
 # Send a message in that 1:1 chat
 New-MgChatMessage -chatId $chatId -BodyContent "Hi from VSCode again!"
+
+# Mention a user in a channel message.
+$UserToMention = Get-MGUser -UserId $userUPN
+
+New-MgTeamChannelMessage -ChannelId $ChannelId -TeamId $TeamId `
+    -BodyContentType "html" `
+    -BodyContent "Welcome to the channel! <at id='0'>$($UserToMention.DisplayName)</at>" `
+    -Mentions @( `
+        @{ `
+            id          = 0; `
+            mentionText = $UserToMention.DisplayName; `
+            mentioned   = @{user = $UserToMention } `
+        })
