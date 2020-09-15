2bf33d7710f50472e6a8a7f3905440104f7246ca
George <gendungu@microsoft.com>
2020-09-15 05:52:22 +0300

    pass repository name

--- tools/GenerateRollUpModule.ps1
+++ tools/GenerateRollUpModule.ps1
@@ -71,7 +71,7 @@ elseif ($VersionState.Equals([VersionState]::Valid) -or $VersionState.Equals([Ve
     }
 
     # Add auth module as a dependency.
-    $ExistingAuthModule = Find-Module "Microsoft.Graph.Authentication" -RepositoryName
+    $ExistingAuthModule = Find-Module "Microsoft.Graph.Authentication" -Repository $RepositoryName
     Install-Module $ExistingAuthModule.Name -Repository $RepositoryName -AllowPrerelease -Force
     $RequiredGraphModules += @{ ModuleName = $ExistingAuthModule.Name ; ModuleVersion = $ExistingAuthModule.Version }
 
