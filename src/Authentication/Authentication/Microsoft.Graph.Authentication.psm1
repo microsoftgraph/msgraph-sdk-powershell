95b62dbfdff35df5b8e87b2dbdb621e64fc07a15
George <1641829+finsharp@users.noreply.github.com>
2021-03-22 15:32:32 +0300

    Change script path

--- src/Authentication/Authentication/Microsoft.Graph.Authentication.psm1
+++ src/Authentication/Authentication/Microsoft.Graph.Authentication.psm1
@@ -1,10 +1,10 @@
 
 # Load the module dll
-$null = Import-Module -Name (Join-Path $PSScriptRoot 'Microsoft.Graph.Authentication.dll')
+$null = Import-Module -Name (Join-Path $PSScriptRoot '.\artifacts\Microsoft.Graph.Authentication.dll')
 
-if (Test-Path -Path "$PSScriptRoot\StartupScripts" -ErrorAction Ignore)
+if (Test-Path -Path "$PSScriptRoot\artifacts\StartupScripts" -ErrorAction Ignore)
 {
-    Get-ChildItem "$PSScriptRoot\StartupScripts" -ErrorAction Stop | ForEach-Object {
+    Get-ChildItem "$PSScriptRoot\artifacts\StartupScripts" -ErrorAction Stop | ForEach-Object {
         . $_.FullName
     }
 }
