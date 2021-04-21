492eba19b563b77122d723317a6261ca59f23511
Mark Wahl <mwahl@microsoft.com>
2021-04-21 10:50:04 -0700

    add get assignment requests

---
+++ src/Identity.Governance/Identity.Governance/custom/Get-MgEntitlementManagementAccessPackageAssignmentRequest.ps1
@@ -0,0 +1,181 @@
+
+# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+<#
+.Synopsis
+Get accessPackageAssignmentRequests from identityGovernance
+.Description
+Get accessPackageAssignmentRequests from identityGovernance
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+
+.Inputs
+Microsoft.Graph.PowerShell.Models.IIdentityGovernanceIdentity
+.Outputs
+Microsoft.Graph.PowerShell.Models.IMicrosoftGraphAccessPackageAssignmentRequest
+.Notes
+
+.Link
+https://docs.microsoft.com/en-us/powershell/module/microsoft.graph.identity.governance/get-mgentitlementmanagementaccesspackageassignmentrequest
+#>
+function Get-MgEntitlementManagementAccessPackageAssignmentRequest {
+[OutputType([Microsoft.Graph.PowerShell.Models.IMicrosoftGraphAccessPackageAssignmentRequest])]
+[CmdletBinding(DefaultParameterSetName='ListAll', PositionalBinding=$false)]
+[Microsoft.Graph.PowerShell.Profile('v1.0-beta')]
+param(
+    [Parameter()]
+    [Alias('Expand')]
+    [Microsoft.Graph.PowerShell.Category('Query')]
+    [System.String[]]
+    # Expand related entities
+    ${ExpandProperty},
+
+    [Parameter()]
+    [Alias('Select')]
+    [Microsoft.Graph.PowerShell.Category('Query')]
+    [System.String[]]
+    # Select properties to be returned
+    ${Property},
+
+    [Parameter(ParameterSetName='AccessPackageId', Mandatory)]
+    [Microsoft.Graph.PowerShell.Category('Query')]
+    [ValidateScript( {
+        try {
+            [System.Guid]::Parse($_) | Out-Null
+            $true
+        }
+        catch {
+            throw "$_ is not a valid ObjectID format. Valid value is a GUID format only."
+        }
+    })]
+    [System.String]
+    # Filter items by property values
+    ${AccessPackageId},
+
+    [Parameter(ParameterSetName='ListAll')]
+    [Parameter(ParameterSetName='AccessPackageId')]
+    [Alias('OrderBy')]
+    [Microsoft.Graph.PowerShell.Category('Query')]
+    [System.String[]]
+    # Order items by property values
+    ${Sort},
+
+    [Parameter(ParameterSetName='AccessPackageId')]
+    [Alias('Limit')]
+    [Microsoft.Graph.PowerShell.Category('Query')]
+    [System.Int32]
+    # Show only the first n items
+    ${Top},
+
+    [Parameter(DontShow)]
+    [Microsoft.Graph.PowerShell.Category('Runtime')]
+    [System.Management.Automation.SwitchParameter]
+    # Wait for .NET debugger to attach
+    ${Break},
+
+    [Parameter(DontShow)]
+    [ValidateNotNull()]
+    [Microsoft.Graph.PowerShell.Category('Runtime')]
+    [Microsoft.Graph.PowerShell.Runtime.SendAsyncStep[]]
+    # SendAsync Pipeline Steps to be appended to the front of the pipeline
+    ${HttpPipelineAppend},
+
+    [Parameter(DontShow)]
+    [ValidateNotNull()]
+    [Microsoft.Graph.PowerShell.Category('Runtime')]
+    [Microsoft.Graph.PowerShell.Runtime.SendAsyncStep[]]
+    # SendAsync Pipeline Steps to be prepended to the front of the pipeline
+    ${HttpPipelinePrepend},
+
+    [Parameter(DontShow)]
+    [Microsoft.Graph.PowerShell.Category('Runtime')]
+    [System.Uri]
+    # The URI for the proxy server to use
+    ${Proxy},
+
+    [Parameter(DontShow)]
+    [ValidateNotNull()]
+    [Microsoft.Graph.PowerShell.Category('Runtime')]
+    [System.Management.Automation.PSCredential]
+    # Credentials for a proxy server to use for the remote call
+    ${ProxyCredential},
+
+    [Parameter(DontShow)]
+    [Microsoft.Graph.PowerShell.Category('Runtime')]
+    [System.Management.Automation.SwitchParameter]
+    # Use the default credentials for the proxy
+    ${ProxyUseDefaultCredentials},
+
+    [Parameter(ParameterSetName='ListAll')]
+    [Parameter(ParameterSetName='AccessPackageId')]
+    [Microsoft.Graph.PowerShell.Category('Runtime')]
+    [System.Management.Automation.SwitchParameter]
+    # List all pages.
+    ${All}
+
+)
+
+begin {
+    try {
+        $outBuffer = $null
+        if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer)) {
+            $PSBoundParameters['OutBuffer'] = 1
+        }
+        $parameterSet = $PSCmdlet.ParameterSetName
+        if ($parameterSet -eq "AccessPackageId") {
+
+            $Filter = "accessPackage/Id eq '{0}'" -f $AccessPackageId
+            $PSBoundParameters['Filter'] = $Filter
+            $null = $PSBoundParameters.Remove('AccessPackageId')
+        }
+        
+        if ($PSBoundParameters.ContainsKey('Top') -or $PSBoundParameters.ContainsKey('All')) {
+
+        } else {
+            $PSBoundParameters['All'] = $true
+        }
+
+        $mappedCmdList = 'Microsoft.Graph.Identity.Governance.private\Get-MgEntitlementManagementAccessPackageAssignmentRequest_List';
+        $subWrappedCmd = $ExecutionContext.InvokeCommand.GetCommand(($mappedCmdList), [System.Management.Automation.CommandTypes]::Cmdlet)
+        $subScriptCmd = {& $subWrappedCmd @PSBoundParameters}
+        $steppablePipeline = $subScriptCmd.GetSteppablePipeline($MyInvocation.CommandOrigin)
+        $steppablePipeline.Begin($PSCmdlet)
+    } catch {
+        throw
+    }
+}
+
+process {
+    try {
+        $steppablePipeline.Process($_)
+    } catch {
+        throw
+    }
+}
+
+end {
+    try {
+        $steppablePipeline.End()
+    } catch {
+        throw
+    }
+}
+}
