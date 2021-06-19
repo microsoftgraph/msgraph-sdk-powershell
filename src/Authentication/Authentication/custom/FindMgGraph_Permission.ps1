b355f9df1af9d308cec4fc17811c0c6e507e9efa
Adam Edwards <adamedx@gmail.com>
2021-06-19 00:12:55 -0700

    Generate empty Find-MgGraphPermission command

---
+++ src/Authentication/Authentication/custom/FindMgGraph_Permission.ps1
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
+Set-StrictMode -Version 2
+
+<#
+.SYNOPSIS
+Find permissions for authorization against Microsoft Graph
+#>
+function Find-MgGraphPermission {
+    [CmdletBinding(positionalbinding=$false)]
+    param()
+    'Hello World!'
+}
+
