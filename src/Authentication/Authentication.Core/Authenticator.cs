b0168b83f881d989860bd42d4cad9672ef350d4b
Peter Ombwa <peter.ombwa@microsoft.com>
2021-06-22 16:50:50 -0700

    Retry on invalid_scopes error code.

--- src/Authentication/Authentication.Core/Authenticator.cs
+++ src/Authentication/Authentication.Core/Authenticator.cs
@@ -32,98 +32,116 @@ namespace Microsoft.Graph.Authentication.Core
         /// <returns></returns>
         public static async Task<IAuthContext> AuthenticateAsync(IAuthContext authContext, bool forceRefresh, CancellationToken cancellationToken, Action fallBackWarning = null)
         {
-            // Gets a static instance of IAuthenticationProvider when the client app hasn't changed.
-            var authProvider = AuthenticationHelpers.GetAuthProvider(authContext);
-            IClientApplicationBase clientApplication = null;
-            switch (authContext.AuthProviderType)
+            int retryCount = 0;
+            do
             {
-                case AuthProviderType.DeviceCodeProvider:
-                case AuthProviderType.DeviceCodeProviderFallBack:
-                    clientApplication = (authProvider as DeviceCodeProvider).ClientApplication;
-                    break;
-                case AuthProviderType.InteractiveAuthenticationProvider:
-                    {
-                        var interactiveProvider = (authProvider as InteractiveAuthenticationProvider).ClientApplication;
-                        //When User is not Interactive, Pre-Emptively Fallback and warn, to DeviceCode
-                        if (!interactiveProvider.IsUserInteractive())
-                        {
-                            authContext.AuthProviderType = AuthProviderType.DeviceCodeProviderFallBack;
-                            fallBackWarning?.Invoke();
-                            var fallBackAuthContext= await AuthenticateAsync(authContext, forceRefresh, cancellationToken, fallBackWarning);
-                            return fallBackAuthContext;
-                        }
-                        break;
-                    }
-                case AuthProviderType.ClientCredentialProvider:
-                    {
-                        clientApplication = (authProvider as ClientCredentialProvider).ClientApplication;
-                        break;
-                    }
-            }
-            try
-            {
-                // Incremental scope consent without re-instantiating the auth provider. We will use provided instance.
-                GraphRequestContext graphRequestContext = new GraphRequestContext();
-                graphRequestContext.CancellationToken = cancellationToken;
-                graphRequestContext.MiddlewareOptions = new Dictionary<string, IMiddlewareOption>
+                // Gets a static instance of IAuthenticationProvider when the client app hasn't changed.
+                var authProvider = AuthenticationHelpers.GetAuthProvider(authContext);
+                IClientApplicationBase clientApplication = null;
+                switch (authContext.AuthProviderType)
                 {
-                    {
-                        typeof(AuthenticationHandlerOption).ToString(),
-                        new AuthenticationHandlerOption
+                    case AuthProviderType.DeviceCodeProvider:
+                    case AuthProviderType.DeviceCodeProviderFallBack:
+                        clientApplication = (authProvider as DeviceCodeProvider).ClientApplication;
+                        break;
+                    case AuthProviderType.InteractiveAuthenticationProvider:
                         {
-                            AuthenticationProviderOption = new AuthenticationProviderOption
+                            var publicClientApp = (authProvider as InteractiveAuthenticationProvider).ClientApplication;
+                            // When user is not interactive, pre-emptively fallback to device code and warn.
+                            if (!publicClientApp.IsUserInteractive())
                             {
-                                Scopes = authContext.Scopes,
-                                ForceRefresh = forceRefresh
+                                authContext.AuthProviderType = AuthProviderType.DeviceCodeProviderFallBack;
+                                fallBackWarning?.Invoke();
+                                var fallBackAuthContext = await AuthenticateAsync(authContext, forceRefresh, cancellationToken, fallBackWarning);
+                                return fallBackAuthContext;
+                            }
+                            clientApplication = publicClientApp;
+                            break;
+                        }
+                    case AuthProviderType.ClientCredentialProvider:
+                        {
+                            clientApplication = (authProvider as ClientCredentialProvider).ClientApplication;
+                            break;
+                        }
+                }
+                try
+                {
+                    // Incremental scope consent without re-instantiating the auth provider. We will use provided instance.
+                    GraphRequestContext graphRequestContext = new GraphRequestContext();
+                    graphRequestContext.CancellationToken = cancellationToken;
+                    graphRequestContext.MiddlewareOptions = new Dictionary<string, IMiddlewareOption>
+                    {
+                        {
+                            typeof(AuthenticationHandlerOption).ToString(),
+                            new AuthenticationHandlerOption
+                            {
+                                AuthenticationProviderOption = new AuthenticationProviderOption
+                                {
+                                    Scopes = authContext.Scopes,
+                                    ForceRefresh = forceRefresh
+                                }
                             }
                         }
+                    };
+
+                    // Trigger consent.
+                    HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/me");
+                    httpRequestMessage.Properties.Add(typeof(GraphRequestContext).ToString(), graphRequestContext);
+                    await authProvider.AuthenticateRequestAsync(httpRequestMessage);
+
+                    IAccount account = null;
+                    if (clientApplication != null)
+                    {
+                        // Only get accounts when we are using MSAL to get an access token.
+                        IEnumerable<IAccount> accounts = clientApplication.GetAccountsAsync().GetAwaiter().GetResult();
+                        account = accounts.FirstOrDefault();
                     }
-                };
 
-                // Trigger consent.
-                HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/v1.0/me");
-                httpRequestMessage.Properties.Add(typeof(GraphRequestContext).ToString(), graphRequestContext);
-                await authProvider.AuthenticateRequestAsync(httpRequestMessage);
+                    JwtHelpers.DecodeJWT(httpRequestMessage.Headers.Authorization?.Parameter, account, ref authContext);
 
-                IAccount account = null;
-                if (clientApplication != null)
-                {
-                    // Only get accounts when we are using MSAL to get an access token.
-                    IEnumerable<IAccount> accounts = clientApplication.GetAccountsAsync().GetAwaiter().GetResult();
-                    account = accounts.FirstOrDefault();
-                }
-
-                JwtHelpers.DecodeJWT(httpRequestMessage.Headers.Authorization?.Parameter, account, ref authContext);
-                return authContext;
-            }
-            catch (AuthenticationException authEx)
-            {
-                //Interactive Authentication Failure: Could Not Open Browser, fallback to DeviceAuth
-                if (IsUnableToOpenWebPageError(authEx))
-                {
-                    authContext.AuthProviderType = AuthProviderType.DeviceCodeProviderFallBack;
-                    //ReAuthenticate using DeviceCode as fallback.
-                    var fallBackAuthContext = await AuthenticateAsync(authContext, forceRefresh, cancellationToken);
-                    //Indicate that this was a Fallback
-                    if (fallBackWarning != null && fallBackAuthContext.AuthProviderType == AuthProviderType.DeviceCodeProviderFallBack)
+                    // Indicate that this was a fallback.
+                    if (fallBackWarning != null && authContext.AuthProviderType == AuthProviderType.DeviceCodeProviderFallBack)
                     {
                         fallBackWarning();
                     }
-                    return fallBackAuthContext;
+                    break;
                 }
-                // DeviceCode Authentication Failure: Timeout
-                if (authEx.InnerException is TaskCanceledException && cancellationToken.IsCancellationRequested)
+                catch (AuthenticationException authEx)
                 {
-                    // DeviceCodeTimeout
-                    throw new Exception(string.Format(CultureInfo.CurrentCulture, ErrorConstants.Message.DeviceCodeTimeout, Constants.MaxDeviceCodeTimeOut));
+                    if (IsUnableToOpenWebPageError(authEx))
+                    {
+                        // Interactive Authentication Failure: Could Not Open Browser, fallback to device code.
+                        authContext.AuthProviderType = AuthProviderType.DeviceCodeProviderFallBack;
+                        retryCount++;
+                    }
+                    else if (authEx.InnerException is MsalServiceException msalServiceEx
+                        && msalServiceEx.StatusCode == 400
+                        && msalServiceEx.ErrorCode == "invalid_scope"
+                        && string.IsNullOrWhiteSpace(authContext.TenantId))
+                    {
+                        // Retry when errorCode is invalid_scope and tenantId is null by setting the authority to 'organizations'.
+                        // See https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/issues/1631.
+                        authContext.TenantId = "organizations";
+                        retryCount++;
+                    }
+                    else if (authEx.InnerException is TaskCanceledException && cancellationToken.IsCancellationRequested)
+                    {
+                        // Authentication requets timeout.
+                        throw new Exception(string.Format(CultureInfo.CurrentCulture, ErrorConstants.Message.DeviceCodeTimeout, Constants.MaxDeviceCodeTimeOut));
+                    }
+                    else
+                    {
+                        //Something unknown went wrong.
+                        throw authEx.InnerException ?? authEx;
+                    }
                 }
-                //Something Unknown Went Wrong
-                throw authEx.InnerException ?? authEx;
-            }
-            catch (Exception ex)
-            {
-                throw ex.InnerException ?? ex;
-            }
+                catch (Exception ex)
+                {
+                    throw ex.InnerException ?? ex;
+                }
+            } while (retryCount <= 1);
+
+            return authContext;
         }
 
         /// <summary>
