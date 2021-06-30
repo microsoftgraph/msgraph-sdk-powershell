fc2f77f5a3162fcfbe2f0ab3ba7c1bd2c2248ade
Taofeek F. Obafemi-Babatunde <taofeekobafemibabatunde@gmail.com>
2021-06-30 18:12:37 -0400

    Improving output to nrea-final output format

--- src/Authentication/Authentication/Microsoft.Graph.Authentication.psd1
+++ src/Authentication/Authentication/Microsoft.Graph.Authentication.psd1
@@ -127,6 +127,7 @@ PrivateData = @{
 
 # HelpInfo URI of this module
 # HelpInfoURI = ''
+
 # Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
 # DefaultCommandPrefix = ''
 
--- src/Authentication/Authentication/custom/FindMgGraph_Permission.ps1
+++ src/Authentication/Authentication/custom/FindMgGraph_Permission.ps1
@@ -11,92 +11,9 @@ Find permissions for authorization against Microsoft Graph
 
 . "$psscriptroot/common/Permissions.ps1"
 
-# 1. Signing into any AAD organization
+# Signing into any AAD organization
 
 # Import-Module -Force .\Microsoft.Graph.Authentication.psd1
-
-# Setting a empty entity to store the imported data
-$msgraphServicePrincipal = $null
-
-function GetPermissionsData {
-
-    # 2. Making a REST request to MS Graph
-    if ($null -eq $script:Permissions_MsGraphServicePrincipal){
-        $script:Permissions_MsGraphServicePrincipal = try {
-
-            Write-Host "Getting data from web service"
-            Invoke-MgGraphRequest -method GET 'https://graph.microsoft.com/v1.0/servicePrincipals?filter=appId eq ''00000003-0000-0000-c000-000000000000''' | select-object -expandproperty value
-
-        } catch {
-
-            Write-Host "Getting data from local file"
-            Get-Content ~/datasample.json | Out-String | ConvertFrom-Json
-        
-        }
-    }
-    # 3. Parse the permisions from the serviceprincipal
-    $msOauth = $script:Permissions_MsGraphServicePrincipal.oauth2PermissionScopes
-    $msAppRoles = $script:Permissions_MsGraphServicePrincipal.appRoles
-
-    # make sure the parsed permissions are exported properly
-    @{
-        oauth2 = $msOauth;
-        appRoles = $msAppRoles
-    }
-
-}
-
-# Search based on user input
-function GetOauthData {
-
-    $permissions = GetPermissionsData
-    $msOauth = $permissions.oauth2
-    
-    ForEach ($oauth2grant in $msOauth) {
-
-        $description = $null
-
-        $description = If ($oauth2grant.type -eq "Admin") { 
-        
-            $description = $oauth2grant.adminConsentDescription
-        
-        } elseif ($oauth2grant.type -eq "User") {
-        
-            $description = $oauth2grant.userConsentDescription
-        
-        }
-        
-        $entry = [ordered] @{
-            
-            "Id" = $oauth2grant.id
-            "PermissionType" = $oauth2grant.type
-            "Name" = $oauth2grant.value
-            "Description" = $description
-        
-        }
-
-        [PSCustomObject] $entry
-
-    }
-}
-
-function GetAppRolesData {
-    
-    $permissions = GetPermissionsData
-    $msAppRoles = $permissions.appRoles 
-    
-    ForEach ($oauth2grant in $msAppRoles) {
-
-        $entry = [ordered] @{
-            "Id" = $oauth2grant.id
-            "PermissionType" = $oauth2grant.origin
-            "Name" = $oauth2grant.value
-            "Description" = $oauth2grant.description
-        }
-        [PSCustomObject] $entry
-
-    }
-}
 
 function Find-MgGraphPermission($search) {
     
