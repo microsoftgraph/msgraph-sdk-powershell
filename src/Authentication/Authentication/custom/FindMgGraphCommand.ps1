f96d3eef191cbed8af0f4f7558d5b444264dab60
Peter Ombwa <peter.ombwa@microsoft.com>
2021-07-12 16:21:45 -0700

    Remove /scripts

--- src/Authentication/Authentication/custom/FindMgGraphCommand.ps1
+++ src/Authentication/Authentication/custom/FindMgGraphCommand.ps1
@@ -7,86 +7,106 @@ Set-StrictMode -Version 2
 
 function Find-MgGraphCommand {
     [OutputType([System.Management.Automation.PSCustomObject])]
-    [CmdletBinding(DefaultParameterSetName = 'FindByUrl', PositionalBinding=$false)]
+    [CmdletBinding(DefaultParameterSetName = 'FindByUrl', PositionalBinding = $false)]
     param (
         [Parameter(ParameterSetName = "FindByUrl", Mandatory = $true)]
-        [string]
-        $Url,
+        [string]$Url,
 
-        [Parameter(ParameterSetName = "FindByUrl", Mandatory = $true)]
+        [Parameter(ParameterSetName = "FindByUrl")]
         [ValidateSet("GET", "POST", "PUT", "PATCH", "DELETE")]
-        [string]
-        $Method = "GET",
+        [string]$Method = "GET",
 
         [Parameter(ParameterSetName = "FindByUrl")]
         [Parameter(ParameterSetName = "FindByCommand")]
         [Alias("Profile")]
-        [string]
-        $ApiVersion,
+        [string]$ApiVersion,
 
         [Parameter(ParameterSetName = "FindByCommand", Mandatory = $true)]
-        [string]
-        $Command
+        [string]$Command
     )
 
-    $MgCommandMetadataFile = (Join-Path $PSScriptRoot "..\..\..\..\assets\MgCommandMetadata.json")
-    if (!(Test-Path $MgCommandMetadataFile)) {
-        Write-Error "MgCommandMetadata file not found."
+    begin {
+
     }
-    $MgCommandMetadata = Get-Content -path $MgCommandMetadataFile | ConvertFrom-Json -AsHashtable
-    $FindMatch = [System.Collections.ArrayList]@()
-    if ($PSCmdlet.ParameterSetName -eq "FindByUrl") {
-        if ($Url -match "https:\/\/graph.microsoft.com\/(v1.0|beta)(\/.*)(\?(.*))?") {
-            $ApiVersion = $matches[1]
-            $ResourceSegement = $matches[2]
-            # TODO: Handle query parameters.
-            $QueryParameters = $matches[3]
-            $RegexResourceSegement = "^$($ResourceSegement -Replace '(?<={)(.*?)(?=})', '.*')$"
+
+    process {
+        $MgCommandMetadataFile = (Join-Path $PSScriptRoot "..\..\..\..\assets\MgCommandMetadata.json")
+        if (!(Test-Path $MgCommandMetadataFile)) {
+            Write-Error "MgCommandMetadata file not found."
+        }
+        $MgCommandMetadata = Get-Content -path $MgCommandMetadataFile | ConvertFrom-Json -AsHashtable
+        $Result = [System.Collections.ArrayList]@()
+        if ($PSCmdlet.ParameterSetName -eq "FindByUrl") {
+            #TODO: Trim trailing "/" in URLs.
+            #TODO: Tokenize URLs with integers by replacing the substring that has the integers with {id}, e.g, /users/289ee2a5-9450-4837-aa87-6bd8d8e72891 -> users/{id}
+            if ($Url -match "https:\/\/graph.microsoft.com\/(v1.0|beta)(\/.*)(\?(.*))?") {
+                $ApiVersion = $matches[1]
+                $ResourceSegement = $matches[2]
+                # TODO: Handle query parameters.
+                $QueryParameters = $matches[3]
+                $RegexResourceSegement = "^$($ResourceSegement -Replace '(?<={)(.*?)(?=})', '(\w*-\w*|\w*)')$"
+            }
+            else {
+                Write-Error "The provided URL doesn't match the expected URL format. Please ensure that the URL is formated as https://graph.microsoft.com/{api-version}/{resource}."
+            }
+            Write-Host "REGEX: $RegexResourceSegement" -ForegroundColor yellow
+
+            $MgCommandMetadata.GetEnumerator() | ForEach-Object {
+                $CommandPath = [PSCustomObject]@{
+                    Command    = $_.Value.Command
+                    Variants   = $_.Value.Variants
+                    URL        = $_.Value.Url
+                    Method     = $_.Value.Method
+                    APIVersion = $_.Value.ApiVersion
+                    OutputType = $_.Value.OutputType
+                }
+                if ($CommandPath.Method -eq $Method -and
+                    $CommandPath.ApiVersion -eq $ApiVersion -and
+                    $CommandPath.Url -match $RegexResourceSegement) {
+                    $null = $Result.Add($CommandPath)
+                }
+            }
+            if ($Result.Count -lt 1) {
+                Write-Error "URL '$Method $Url' in $ApiVersion is not valid or is not currently supported by the SDK. Please file an issue here https://aka.ms/msgraph-ps-bug for this to be added."
+            }
         }
         else {
-            Write-Error "The provided URL doesn't match the expected URL format. Please ensure that the URL is formated as https://graph.microsoft.com/{api-version}/{resource}."
-        }
-        Write-Host "REGEX: $RegexResourceSegement" -ForegroundColor yellow
+            $MgCommandMetadata.GetEnumerator() | ForEach-Object {
+                $CommandPath = [PSCustomObject]@{
+                    Command    = $_.Value.Command
+                    Variants   = $_.Value.Variants
+                    URL        = $_.Value.Url
+                    Method     = $_.Value.Method
+                    APIVersion = $_.Value.ApiVersion
+                    OutputType = $_.Value.OutputType
+                }
+                if ($CommandPath.ApiVersion -eq $ApiVersion -and
+                    $CommandPath.Command -eq $Command) {
+                    $null = $Result.Add($CommandPath)
+                }
+            }
 
-        $MgCommandMetadata.GetEnumerator() | ForEach-Object {
-            $CommandPath = $_.Value
-            if ($CommandPath.Method -eq $Method -and
-                $CommandPath.ApiVersion -eq $ApiVersion -and
-                $CommandPath.Url -match $RegexResourceSegement) {
-                $null = $FindMatch.Add($CommandPath)
+            if ($Result.Count -lt 1) {
+                Write-Error "‘$Command’ is not a valid Microsoft Graph PowerShell command. Please check the name and try again."
             }
         }
-    }
-    else {
-        $MgCommandMetadata.GetEnumerator() | ForEach-Object {
-            $CommandPath = $_.Value
-            if ($CommandPath.ApiVersion -eq $ApiVersion -and
-                $CommandPath.Command -eq $Command) {
-                $null = $FindMatch.Add($CommandPath)
-            }
-        }
+
+        Write-Output $Result
     }
 
-    if ($FindMatch.Count -ge 1) {
-        Write-Host "Command: $($FindMatch[0].Command)" -ForegroundColor green
-        Write-Host "Module: Microsoft.Graph.$($FindMatch[0].Module)" -ForegroundColor green
-        Write-Host "Profile: $($FindMatch[0].ApiVersion)" -ForegroundColor green
-        Write-Host "Returns: $($FindMatch[0].OutputType)" -ForegroundColor green
-        Write-Host "Url: $($FindMatch[0].Url)" -ForegroundColor green
-    }
-    else {
-        Write-Warning "Command not found!"
+    end {
+
     }
 }
 
-    # TEST DATA
-    # .\FindMgCommand.ps1 -Command "Get-MgUser" -ApiVersion "beta"
+# TEST DATA
+# .\FindMgCommand.ps1 -Command "Get-MgUser" -ApiVersion "beta"
 
-    # .\FindMgCommand.ps1 -Url "https://graph.microsoft.com/v1.0/users/{id}/microsoft.graph.exportPersonalData" -Method "POST"
-    # .\FindMgCommand.ps1 -Url "https://graph.microsoft.com/beta/identityGovernance/entitlementManagement/accessPackageCatalogs" -Method POST
-    # .\FindMgCommand.ps1 -Url "https://graph.microsoft.com/v1.0/users" -Method GET
-    # .\FindMgCommand.ps1 -Url https://graph.microsoft.com/beta/identityGovernance/entitlementManagement/accessPackageAssignmentResourceRoles/{id} -Method GET
-    # .\FindMgCommand.ps1 -Url https://graph.microsoft.com/beta/identityGovernance/entitlementManagement/accessPackageCatalogs -Method POST
+# .\FindMgCommand.ps1 -Url "https://graph.microsoft.com/v1.0/users/{id}/microsoft.graph.exportPersonalData" -Method "POST"
+# .\FindMgCommand.ps1 -Url "https://graph.microsoft.com/beta/identityGovernance/entitlementManagement/accessPackageCatalogs" -Method POST
+# .\FindMgCommand.ps1 -Url "https://graph.microsoft.com/v1.0/users" -Method GET
+# .\FindMgCommand.ps1 -Url https://graph.microsoft.com/beta/identityGovernance/entitlementManagement/accessPackageAssignmentResourceRoles/{id} -Method GET
+# .\FindMgCommand.ps1 -Url https://graph.microsoft.com/beta/identityGovernance/entitlementManagement/accessPackageCatalogs -Method POST
 
 
-    # .\FindMgCommand.ps1 -Url "https://graph.microsoft.com/v1.0/users?$select=displayName,id&$filter=identities/any(c:c/issuerAssignedId eq 'j.smith@yahoo.com' and c/issuer eq 'contoso.onmicrosoft.com')" -Method GET
+# .\FindMgCommand.ps1 -Url "https://graph.microsoft.com/v1.0/users?$select=displayName,id&$filter=identities/any(c:c/issuerAssignedId eq 'j.smith@yahoo.com' and c/issuer eq 'contoso.onmicrosoft.com')" -Method GET
