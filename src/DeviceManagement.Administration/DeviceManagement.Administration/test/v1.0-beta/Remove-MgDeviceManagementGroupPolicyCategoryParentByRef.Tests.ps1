705887696803f17d2b9daed8716f831646eb1bbe
George <gendungu@microsoft.com>
2021-07-13 00:49:04 +0300

    Test

---
+++ src/DeviceManagement.Administration/DeviceManagement.Administration/test/v1.0-beta/Remove-MgDeviceManagementGroupPolicyCategoryParentByRef.Tests.ps1
@@ -0,0 +1,22 @@
+$loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+if (-Not (Test-Path -Path $loadEnvPath)) {
+    $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+}
+. ($loadEnvPath)
+$TestRecordingFile = Join-Path $PSScriptRoot 'Remove-MgDeviceManagementGroupPolicyCategoryParentByRef.Recording.json'
+$currentPath = $PSScriptRoot
+while(-not $mockingPath) {
+    $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+    $currentPath = Split-Path -Path $currentPath -Parent
+}
+. ($mockingPath | Select-Object -First 1).FullName
+
+Describe 'Remove-MgDeviceManagementGroupPolicyCategoryParentByRef' {
+    It 'Delete' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'DeleteViaIdentity' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
