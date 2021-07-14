50a4dce709144bae75b25f39ff1c53166efa2210
Taofeek F. Obafemi-Babatunde <taofeekobafemibabatunde@gmail.com>
2021-07-14 13:42:45 -0400

    testing

--- src/Authentication/Authentication/test/Find-MgGraphPermission.Tests.ps1
+++ src/Authentication/Authentication/test/Find-MgGraphPermission.Tests.ps1
@@ -2,13 +2,27 @@
 # Copyright (c) Microsoft Corporation. All Rights Reserved. Licensed under the MIT License. See License in the project root for license information.
 # ------------------------------------------------------------------------------
 Describe "the Find-MgGraphPermission Command" {
-    BeforeAll{
+
+    BeforeAll {
         . (join-path $PSScriptRoot  ..\custom\Find-MgGraphPermission.ps1)
     }
 
     Context "when executing the command" {
+        BeforeAll {
+            Mock Invoke-MgGraphRequest {
+
+                [PSCustomObject] @{
+                    Value = [PSCustomObject] @{
+                        Oauth2PermissionScopes=@();
+                        AppRoles=@()
+                    }
+                }
+            }
+        }
+
         It 'Executes successfully with no parameters' {
             { Find-MgGraphPermission | Out-Null } | Should -Not -Throw
+            Assert-MockCalled Invoke-MgGraphRequest
         }
 
     }
