7bdf83cb7f26693aa2698b2299bec3d4a4606020
Adam Edwards <adamed@microsoft.com>
2021-08-05 11:04:07 -0400

    Add help documentation for Find-MgGraphPermission

--- src/Authentication/Authentication/custom/Find-MgGraphPermission.ps1
+++ src/Authentication/Authentication/custom/Find-MgGraphPermission.ps1
@@ -8,7 +8,112 @@ Set-StrictMode -Version 6.0
 
 <#
 .SYNOPSIS
-Find permissions for authorization against Microsoft Graph
+Finds Microsoft Graph permissions based on search criteria.
+
+.DESCRIPTION
+For an application to access Microsoft Graph data, it must be granted consent for permissions to
+access that data by the user of the application or an administrator. Find-MgGraphPermission finds
+such permissions that match specified criteria. It returns information about each matching permission
+including the name of the permission, its unique identifier, and a description of the Microsoft Graph
+data access granted by that permission.
+
+The information returned by this command enables you to quickly identify the permission required
+for a script or application to access the particular types of data through Microsoft Graph. Once you've
+used the command to identify the permissions, you can use the names of those permission as a parameter to
+the Connect-MgGraph command to request the permission so that subsequent PowerShell commands or scripts
+may access the desired data from Microsoft Graph for instance. The unique identifier of a permission may
+also be used to configure consent for an application through additional PowerShell commands or other
+custom applications.
+
+Full details for all of the possible permissions and the access they authorize, including how to ensure
+that your application or script follows the best security pratice of using the least privilege necessary
+are found in the Microsoft Graph Permissions Reference documenation at https://docs.microsoft.com/en-us/graph/permissions-reference.
+
+In order to search for the specified permissions, Find-MgGraphPermission uses Microsoft Graph itself
+to obtain the latest complete set of permissions; if it is not able to access Microsoft Graph, the command
+then uses its most recent copy of the information as the source of data.
+
+Note that the results of Find-MgGraphPermission grouped by Delegated and Application permission types, and then sorted
+by permission name. They are not sorted by least-privileged access, so when multiple permissions appear to satisfy your
+script or application's requirements, consult the Microsoft Graph Permissions Reference documenation to identify the
+least-privileged permission for your use case.
+
+.PARAMETER SearchString
+The SearchString parameter allows you to specify a string such as 'user' or 'mail' that represents the subject or domain
+of the permission you're searching for. Since permissions usually have names such as 'User.Read' or 'Mail.ReadWrite', the
+command uses the SearchString parameter to return all permissions that contain the value specified for SearchString in the
+name of the permission.
+
+.PARAMETER Online
+Specify the Online parameter in addition to SearchString to force Find-MgGraphPermission to update its set of permissions
+by requesting the latest permissions data from Microsoft Graph itself before searching for the permissions specified the
+SearchString parameter. This ensures that Find-MgGraphPermission returns the most accurate search results as new permissions
+are added to Microsoft Graph for new APIs. The command uses the existing access to Microsoft Graph as enabled by a previous
+invocation of the Connect-MgGraph command to issue the request for updated permissions. If your current connection does not
+already have access to read this data from Microsoft Graph or if there is no network connectivity to Microsoft Graph, the command will fail.
+If the command is successful in updating the set of permissions prior to searching for permissions, Find-MgGraphPermission will
+continue to use the updated list for all future invocations of the command even if they do not specify the Online parameter.
+
+.OUTPUTS
+This command returns a collection of items with the following fields:
+* Name: The name of the permission as found in Microsoft Graph permissions reference documentation. Names will typically
+  have a form like 'User.Read.All', or 'Files.ReadWrite' for instance.
+* Description: Provides a description of what access is granted by the permission
+* Id: The unique identifier for the permission in the form of a Guid. The unique identifier is required for using
+  certain Microsoft Graph REST API resources or Microsoft Graph-based PowerShell commands that manage application consent.
+* Consent: Denotes whether the specified permission requires administrator or user consent. The valid values are 'Admin' and 'User'.
+* PermissionType: Valid values are 'Delegated' or 'Application' depending on whether the permission is one that is delegated by
+  the user to the application ('Delegated'), or is directly assigned to the application without the consent of a user ('Application').
+
+.EXAMPLE
+Find-MgGraphPermission
+
+This returns all of the possible permissions for Microsoft Graph.
+
+.EXAMPLE
+PS> Find-MgGraphPermission mailbox
+
+   PermissionType: Delegated
+
+Id                                   Consent Name                      Description
+--                                   ------- ----                      -----------
+87f447af-9fa4-4c32-9dfa-4a57a73d18ce User    MailboxSettings.Read      Allows the app to read your mailbox settings.
+818c620a-27a9-40bd-a6a5-d96f7d610b4b User    MailboxSettings.ReadWrite Allows the app to read, update, create, and del…
+
+   PermissionType: Application
+
+Id                                   Consent Name                      Description
+--                                   ------- ----                      -----------
+40f97065-369a-49f4-947c-6a255697ae91 Admin   MailboxSettings.Read      Allows the app to read user's mailbox settings …
+6931bccd-447a-43d1-b442-00a195474933 Admin   MailboxSettings.ReadWrite Allows the app to create, read, update, and del…
+
+PS> Connect-MgGraph -Scopes MailboxSettings.Read
+
+In this example the value 'mailbox' was specified for the SearchString parameter. The resulting output showed the list
+of permissions grouped by Delegated or Application permission type. By viewing the output, the user is able to see that the
+desired delegated permission is 'MailboxSettings.Read'. The user then invokes the Connect-MgGraph command specifying that
+permission name for the value of the Scopes parameter in order to request the permission as part of a new sign-in.
+Subsequent PowerShell commands issued in the session that access Microsoft Graph will then be authorized with the permission
+if the sign-in was successful.
+
+.EXAMPLE
+Find-MgGraphPermission mailbox | Where-Object PermissionType -eq Delegated | Format-List Name, Description
+
+Name        : MailboxSettings.Read
+Description : Allows the app to read your mailbox settings.
+
+Name        : MailboxSettings.ReadWrite
+Description : Allows the app to read, update, create, and delete your mailbox settings
+
+This example outputs the set of delegated permissions that match the 'mailbox' SearchString parameter. The Where-Object
+command is used to filter the permissions to only the delegated permissions through the PermissionType property. The filtered
+result is piped to the Format-List command so that the output of the Description field is not truncated as it would be
+in the default table view.
+
+.LINK
+Connect-MgGraph
+New-MgGraphApplication
+New-MgServicePrinicipal
 #>
 function Find-MgGraphPermission {
     [cmdletbinding()]
