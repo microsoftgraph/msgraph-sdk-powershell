421fb5fab76f3c41645b8ab764b0b26545e52079
Peter Ombwa <peter.ombwa@microsoft.com>
2021-10-21 12:23:03 -0700

    Tweak ByRef OperationId.

--- tools/UpdateOpenApi.ps1
+++ tools/UpdateOpenApi.ps1
@@ -27,24 +27,49 @@ $DownloadOpenApiDocPS1 = Join-Path $PSScriptRoot ".\DownloadOpenApiDoc.ps1" -Res
 if (-not (Test-Path $ModuleMappingConfigPath)) {
     Write-Error "Module mapping file not be found: $ModuleMappingConfigPath."
 }
-
+$Stopwatch = [system.diagnostics.stopwatch]::StartNew()
 [HashTable] $ModuleMapping = Get-Content $ModuleMappingConfigPath | ConvertFrom-Json -AsHashTable
-$ModuleMapping.Keys | ForEach-Object -Begin { $RequestCount = 0 } -End { Write-Host -ForeGroundColor Green "Requests: $RequestCount" } -Process {
-    $ModuleName = $_
-    $ForceRefresh = $false
-    # Check whether ForceRefresh is required, Only required for the First Request.
-    if ($RequestCount -eq 0) {
-        $ForceRefresh = $true
-    }
+# $ModuleMapping.Keys | ForEach-Object -Begin { $RequestCount = 0 } -End { Write-Host -ForeGroundColor Green "Requests: $RequestCount" } -Process {
+#     $ModuleName = $_
+#     $ForceRefresh = $false
+#     # Check whether ForceRefresh is required, Only required for the First Request.
+#     if ($RequestCount -eq 0) {
+#         $ForceRefresh = $true
+#     }
 
-    try {
-        # Download OpenAPI document for module.
-        & $DownloadOpenApiDocPS1 -ModuleName $ModuleName -ModuleRegex $ModuleMapping[$ModuleName] -OpenApiDocOutput $OpenApiDocOutput -GraphVersion $GraphVersion -ForceRefresh:$ForceRefresh -RequestCount $RequestCount
+#     try {
+#         # Download OpenAPI document for module.
+#         & $DownloadOpenApiDocPS1 -ModuleName $ModuleName -ModuleRegex $ModuleMapping[$ModuleName] -OpenApiDocOutput $OpenApiDocOutput -GraphVersion $GraphVersion -ForceRefresh:$ForceRefresh -RequestCount $RequestCount
+#     }
+#     catch {
+#         Write-Error $_.Exception
+#     }
+#     $RequestCount++
+# }
+$stopwatch.Stop()
+Write-Host -ForegroundColor Green "Downloaded $GraphVersion OpenAPI files in '$($Stopwatch.Elapsed.TotalMinutes)` minutes."
+
+# Tweak OperationIds to address known AutoREST.PowerShell limitations.
+$Stopwatch.Restart()
+Get-ChildItem -Path $OpenApiDocOutput | ForEach-Object {
+    $FilePath = $_.FullName
+    $FileContent = Get-Content -Path $FilePath | ConvertFrom-Yaml -Ordered
+    $Modified = $false
+    # Modify '/$ref' operationId (*ByRef) to byPass -> https://github.com/Azure/autorest.powershell/issues/795.
+    $FileContent.paths.Keys -like "*`$ref" | ForEach-Object {
+        $Modified = $true
+        $Path = $_
+        $ActionPattern = [Regex]::new("(_[A-Z][a-z]+)", "Compiled")
+        foreach ($Method in $FileContent.paths[$Path].GetEnumerator()) {
+            $OperationId = $FileContent.paths[$Path][$Method.Name].operationId.Replace("ByRef", "")
+            $Match = $ActionPattern.Match($OperationId)
+            if ($Match -ne $null -and $Match.Success) {
+                $FileContent.paths[$Path][$Method.Name].operationId = ($OperationId -replace $Match.Value, "$($Match.Value)GraphRef")
+            }
+        }
+        if ($Modified) { $FileContent | ConvertTo-Yaml -OutFile $FilePath -Force }
     }
-    catch {
-        Write-Error $_.Exception
-    }
-    $RequestCount++
 }
-
+$stopwatch.Stop()
+Write-Host -ForegroundColor Green "Tweaked $GraphVersion OpenAPI files in '$($Stopwatch.Elapsed.TotalMinutes)` minutes."
 Write-Host -ForegroundColor Green "-------------Done-------------"
