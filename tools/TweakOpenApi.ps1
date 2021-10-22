eebc0b7b4e08c0403efcda8d25f11efe23365f81
Peter Ombwa <peter.ombwa@microsoft.com>
2021-10-21 17:13:34 -0700

    Move logic to tweak OpenAPI files to a new files.

---
+++ tools/TweakOpenApi.ps1
@@ -0,0 +1,40 @@
+﻿# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $OpenAPIFilesPath
+)
+
+$TargetWord = "ByRef"
+$ReplacementWord = "GraphRef"
+$Stopwatch = [system.diagnostics.stopwatch]::StartNew()
+Get-ChildItem -Path $OpenAPIFilesPath | ForEach-Object {
+    $filePath = $_.FullName
+    $fileContent = Get-Content -Path $filePath | ConvertFrom-Yaml -Ordered
+    $modified = $false
+    # Modify '/$ref' operationIds (*ByRef) to byPass -> https://github.com/Azure/autorest.powershell/issues/795.
+    $fileContent.paths.Keys -like "*`$ref" | ForEach-Object {
+        $requestUri = $_
+        # Matches '_{ActionName}' in operationIds formatted as '{entitySet}_{ActionName}{NavigationProperty}'. e.g.,
+        # For 'applications_GetCreatedOnBehalfOfByRef', we will match '_Get'.
+        $ActionPattern = [Regex]::new("(_[A-Z][a-z]+)", "Compiled")
+        foreach ($method in $fileContent.paths[$requestUri].GetEnumerator()) {
+            $operationId = $fileContent.paths[$requestUri][$method.Name].operationId
+            if ($operationId.EndsWith($TargetWord)) {
+                $operationId = $operationId.Replace($TargetWord, "")
+                $match = $ActionPattern.Match($operationId)
+                if ($match -ne $null -and $match.Success) {
+                    # Suffix matched '_{ActionName}' with 'GraphRef'. GraphRef will be our safe unique word for identifying *ByRef commands in PowerShell e.g.,
+                    # 'applications_GetCreatedOnBehalfOf' -> 'applications_GetGraphRefCreatedOnBehalfOf'.
+                    $fileContent.paths[$requestUri][$method.Name].operationId = ($operationId -replace $match.Value, "$($match.Value)$ReplacementWord")
+                    $Modified = $true
+                }
+            }
+        }
+        if ($modified) { $fileContent | ConvertTo-Yaml -OutFile $filePath -Force }
+    }
+}
+$stopwatch.Stop()
+Write-Debug "Tweaked '$OpenAPIFilesPath' OpenAPI files in '$($Stopwatch.Elapsed.TotalMinutes)` minutes."
--- tools/UpdateOpenApi.ps1
+++ tools/UpdateOpenApi.ps1
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+[CmdletBinding()]
 Param(
     [string] $ModuleMappingConfigPath = (Join-Path $PSScriptRoot "..\config\ModulesMapping.jsonc"),
     [string] $OpenApiDocOutput = (Join-Path $PSScriptRoot "..\openApiDocs"),
@@ -11,8 +12,11 @@ $LASTEXITCODE = $null
 if ($PSEdition -ne 'Core') {
     Write-Error 'This script requires PowerShell Core to execute. [Note] Generated cmdlets will work in both PowerShell Core or Windows PowerShell.'
 }
-# Install Powershell-yaml
-Install-Module powershell-yaml -Force
+
+if (!(Get-Module powershell-yaml)){
+    # Install Powershell-yaml
+    Install-Module powershell-yaml -Force
+}
 
 $GraphVersion = "v1.0"
 if ($BetaGraphVersion) {
@@ -21,15 +25,16 @@ if ($BetaGraphVersion) {
 
 $OpenApiDocOutput = Join-Path $OpenApiDocOutput $GraphVersion
 
-# PS Scripts
+# Load PS Scripts
 $DownloadOpenApiDocPS1 = Join-Path $PSScriptRoot ".\DownloadOpenApiDoc.ps1" -Resolve
+$TweakOpenApiPs1 = Join-Path $PSScriptRoot ".\TweakOpenApi.ps1" -Resolve
 
 if (-not (Test-Path $ModuleMappingConfigPath)) {
     Write-Error "Module mapping file not be found: $ModuleMappingConfigPath."
 }
 $Stopwatch = [system.diagnostics.stopwatch]::StartNew()
 [HashTable] $ModuleMapping = Get-Content $ModuleMappingConfigPath | ConvertFrom-Json -AsHashTable
-$ModuleMapping.Keys | ForEach-Object -Begin { $RequestCount = 0 } -End { Write-Host -ForeGroundColor Green "Requests: $RequestCount" } -Process {
+$ModuleMapping.Keys | ForEach-Object -Begin { $RequestCount = 0 } -End { Write-Debug "Requests: $RequestCount" } -Process {
     $ModuleName = $_
     $ForceRefresh = $false
     # Check whether ForceRefresh is required, Only required for the First Request.
@@ -47,29 +52,8 @@ $ModuleMapping.Keys | ForEach-Object -Begin { $RequestCount = 0 } -End { Write-H
     $RequestCount++
 }
 $stopwatch.Stop()
-Write-Host -ForegroundColor Green "Downloaded $GraphVersion OpenAPI files in '$($Stopwatch.Elapsed.TotalMinutes)` minutes."
+Write-Debug "Downloaded $GraphVersion OpenAPI files in '$($Stopwatch.Elapsed.TotalMinutes)` minutes."
 
 # Tweak OperationIds to address known AutoREST.PowerShell limitations.
-$Stopwatch.Restart()
-Get-ChildItem -Path $OpenApiDocOutput | ForEach-Object {
-    $FilePath = $_.FullName
-    $FileContent = Get-Content -Path $FilePath | ConvertFrom-Yaml -Ordered
-    $Modified = $false
-    # Modify '/$ref' operationId (*ByRef) to byPass -> https://github.com/Azure/autorest.powershell/issues/795.
-    $FileContent.paths.Keys -like "*`$ref" | ForEach-Object {
-        $Modified = $true
-        $Path = $_
-        $ActionPattern = [Regex]::new("(_[A-Z][a-z]+)", "Compiled")
-        foreach ($Method in $FileContent.paths[$Path].GetEnumerator()) {
-            $OperationId = $FileContent.paths[$Path][$Method.Name].operationId.Replace("ByRef", "")
-            $Match = $ActionPattern.Match($OperationId)
-            if ($Match -ne $null -and $Match.Success) {
-                $FileContent.paths[$Path][$Method.Name].operationId = ($OperationId -replace $Match.Value, "$($Match.Value)GraphRef")
-            }
-        }
-        if ($Modified) { $FileContent | ConvertTo-Yaml -OutFile $FilePath -Force }
-    }
-}
-$stopwatch.Stop()
-Write-Host -ForegroundColor Green "Tweaked $GraphVersion OpenAPI files in '$($Stopwatch.Elapsed.TotalMinutes)` minutes."
+. $TweakOpenApiPs1 -OpenAPIFilesPath $OpenApiDocOutput
 Write-Host -ForegroundColor Green "-------------Done-------------"
