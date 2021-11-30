fabe7cdd580b16e97a66796748520b61d5115782
George <mbuthiagrg@gmail.com>
2021-11-30 14:42:45 +0300

    Remove unused methods.

--- tools/Utilities/utils.ps1
+++ tools/Utilities/utils.ps1
@@ -1,15 +1,6 @@
 ﻿# Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 
 <#
-       Install Required Tools
-#>
-Function Install-AzModule {
-    if (!(Get-Module -Name Az -ListAvailable)) {
-        Install-Module -Name Az -Repository PSGallery -Scope CurrentUser -Force -SkipPublisherCheck -AllowClobber 
-        Import-Module Az
-    }
-}
-<#
     Connect to the Graph Tenant
 #>
 Function Connect-GraphTenant
@@ -20,42 +11,7 @@ Function Connect-GraphTenant
 }
 
 <#
-    Connect to Azure Tenant to Access KeyVault and other resources.
-#>
-Function Connect-AzureTenant
-{
-    $AzureTenantID = ${env:AZURETENANTID}
-    $AzureApplicationID = ${env:AZUREAPPLICATIONID}
-
-    $securePassword = ConvertTo-SecureString -String $env:AZUREAPPCLIENTSECRET -AsPlainText -Force
-
-    $Credential = New-Object -TypeName PSCredential -ArgumentList $AzureApplicationID, $securePassword
-    Connect-AzAccount -Credential $Credential -Tenant $AzureTenantID -ServicePrincipal | Out-Null
-}
-
-<#
-    Get Certificate from Azure KeyVault
-#>
-$global:DefaultCertificate = $null
-Function Get-Certificate
-{
-    if ($null -eq $global:DefaultCertificate)
-    {
-        Connect-AzureTenant
-        # Certificate must be downloaded as a Secret instead of a Certificate to bring down the PrivateKey as well.
-        $keyVaultCertSecret = Get-AzKeyVaultSecret -VaultName ${env:AZUREKEYVAULTNAME} -Name ${env:CERTIFICATENAME}
-        # Convert the Secret Value in the response to plainText
-        $secureCertData = ConvertFrom-SecureString -SecureString $keyVaultCertSecret.SecretValue -AsPlainText
-        # KeyVault Secrets are Base64 Encoded, thus decode.
-        $base64CertData = [Convert]::FromBase64String($secureCertData)
-        # Create an In-Memory cert from Cert Data
-        $pfxCertificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList @($base64CertData, "", [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable)
-        $global:DefaultCertificate = $pfxCertificate
-    }
-    return $global:DefaultCertificate
-}
-<#
-    Get Certificate from Azure KeyVault
+    Get Certificate from Azure KeyVault loaded via Environment variable
 #>
 Function Get-LocalCertificate
 {
