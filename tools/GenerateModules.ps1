b54b3fbb16561b3d9df30f42fafab52f3d5a7f73
Peter Ombwa <peter.ombwa@microsoft.com>
2022-01-07 16:21:39 -0800

    test autorest path.

--- tools/GenerateModules.ps1
+++ tools/GenerateModules.ps1
@@ -75,12 +75,12 @@ if ($ModulePreviewNumber -eq -1) {
 }
 
 # Build AutoREST.PowerShell submodule.
-$AutoRESTPath = (Join-Path $ScriptRoot "..\autorest.powershell")
+$AutoRESTPath = (Join-Path $ScriptRoot "..\autorest.powershell" -Resolve)
 $AutoRESTPath2 = (Join-Path $PSScriptRoot "..\autorest.powershell")
 Write-Host $AutoRESTPath
 Write-Host $AutoRESTPath2
 Write-Host (Join-Path $ScriptRoot "../autorest.powershell")
-Set-Location $AutoRESTPath
+cd $AutoRESTPath
 rush update
 rush build
 
