ad6172a2e1a3906a2f509400b5ea2e9f257335a8
Timothy Wamalwa <twamalwa@microsoft.com>
2022-06-28 07:40:28 +0300

    ForceRefresh for v1 only

--- tools/UpdateOpenApi.ps1
+++ tools/UpdateOpenApi.ps1
@@ -38,8 +38,8 @@ $ModuleMapping.Keys | ForEach-Object -Begin { $RequestCount = 0 } -End { Write-D
     $ModuleName = $_
     $ForceRefresh = $false
     # Check whether ForceRefresh is required, Only required for the First Request.
-    if ($RequestCount -eq 0) {
-        $ForceRefresh = $false
+    if ($RequestCount -eq 0 -and $GraphVersion -eq "v1.0") {
+        $ForceRefresh = $true
     }
 
     try {
