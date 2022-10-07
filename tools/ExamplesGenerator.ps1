911adf07bc8d77cfff6bd814d0d5c1cbc0394ba3
Timothy Wamalwa <timwamalwa@gmail.com>
2022-10-07 14:17:38 +0300

    Added user scope

--- tools/ExamplesGenerator.ps1
+++ tools/ExamplesGenerator.ps1
@@ -244,7 +244,7 @@ if (-not (Test-Path $ModuleMappingConfigPath)) {
 }
 If (-not (Get-Module -ErrorAction Ignore -ListAvailable PowerHTML)) {
     Write-Verbose "Installing PowerHTML module for the current user..."
-    Install-Module PowerHTML -ErrorAction Stop -Scope AllUsers
+    Install-Module PowerHTML -ErrorAction Stop -Scope CurrentUser -Force
 }
 Import-Module -ErrorAction Stop PowerHTML
 
