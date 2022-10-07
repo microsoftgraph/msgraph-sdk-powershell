e736089256d2468c2438b8122343708d1d1bd5ca
Timothy Wamalwa <timwamalwa@gmail.com>
2022-10-07 12:50:34 +0300

    Corrected location

--- tools/ExamplesGenerator.ps1
+++ tools/ExamplesGenerator.ps1
@@ -247,7 +247,6 @@ if ($ModulesToGenerate.Count -eq 0) {
     [HashTable] $ModuleMapping = Get-Content $ModuleMappingConfigPath | ConvertFrom-Json -AsHashTable
     $ModulesToGenerate = $ModuleMapping.Keys
 }
-Set-Location msgraph-sdk-powershell
 $date = Get-Date -Format "dd-MM-yyyy"
 $proposedBranch = "weekly_examples_update_"+$date
 $exists = git branch -l $proposedBranch
