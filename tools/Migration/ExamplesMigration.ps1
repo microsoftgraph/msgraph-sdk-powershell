a54d248f555efb74f2e3abf3211214637056541b
Timothy Wamalwa <timwamalwa@gmail.com>
2022-12-29 17:23:44 +0300

    Modified examples migration file

--- tools/Migration/ExamplesMigration.ps1
+++ tools/Migration/ExamplesMigration.ps1
@@ -11,21 +11,20 @@ function Get-GraphMapping {
     $graphMapping.Add("beta", "beta")
     return $graphMapping
 }
-function GenerateModules{
-    
-}
 function Start-Copy {
     Param(
         [ValidateNotNullOrEmpty()]
         [string] $Module = "Users"
     )
-    
     $GraphMapping = Get-GraphMapping 
     $GraphMapping.Keys | ForEach-Object {
         $GraphProfile = $_
         Get-FilesByProfile -GraphProfile $GraphProfile -ModulesToGenerate $ModulesToGenerate -Module $Module
     }
+   
 }
+
+
 function Get-FilesByProfile {
     Param(
         [ValidateSet("beta", "v1.0")]
@@ -39,30 +38,38 @@ function Get-FilesByProfile {
     }
     $FromPath = Join-Path $SourceDir $Module $Module "examples" $SourceGraphProfile
     $ToPath = Join-Path "..\..\" "src" $Module $GraphProfile "examples"
-    Copy-Files -SourcePath $FromPath -DestPath $ToPath -GraphProfile $GraphProfile -Module $Module
+    $ConfirmationPath = Join-Path "..\..\" "src" $Module $Module "examples" $SourceGraphProfile
+    Write-Host "Confirm path ----  "$ConfirmationPath
+    Copy-Files -SourcePath $FromPath -DestPath $ToPath -GraphProfile $GraphProfile -Module $Module -ConfirmationPath $ConfirmationPath
 }
 function Copy-Files {
     param(
         [ValidateSet("beta", "v1.0")]
         [string] $GraphProfile = "v1.0",
         [ValidateNotNullOrEmpty()]
-        [string] $SourcePath = "-SourcePath",
+        [string] $SourcePath = "SourcePath",
         [ValidateNotNullOrEmpty()]
-        [string] $DestPath = "-DestPath",
+        [string] $DestPath = "DestPath",
+        [ValidateNotNullOrEmpty()]
+        [string] $ConfirmationPath = "ConfirmationPath",
         [ValidateNotNullOrEmpty()]
         [string] $Module = "Users"
     )
     if ((Test-Path $SourcePath)) {
+        
         foreach ($File in Get-ChildItem $SourcePath) {
             $FileName = [System.IO.Path]::GetFileNameWithoutExtension($File)
             $OriginalFileName = $FileName
             if ($GraphProfile -eq "beta") {
                 $FileName = $FileName.Replace("Mg", "MgBeta")
             }
-            $FileToCheck = Join-Path $DestPath "$FileName.md"
+            $FileToCheck = Join-Path $ConfirmationPath "$FileName.md"
             if (Test-Path $FileToCheck) {
+                Write-Host "Still generating " $FileToCheck
                 if ($GraphProfile -eq "v1.0") {
                     Copy-Item $File -Destination $DestPath
+                    git add $DestPath
+                    git commit -m "Migration for $File done"
                 }
                 else {
                     Remove-Item $FileToCheck
@@ -112,7 +119,9 @@ function update-ImportCommand {
     (Get-Content $FilePath) | 
     Foreach-Object { $_ -replace 'Import-Module Microsoft.Graph', 'Import-Module Microsoft.Graph.Beta' }  | 
     Out-File $FilePath
-
+    $FileName = [System.IO.Path]::GetFileNameWithoutExtension($FilePath)
+    git add $FilePath
+    git commit -m "Migration for $FileName.md done"
 } 
 
 Write-Host -ForegroundColor Green "-------------Fetching docs and examples from dev-------------"
