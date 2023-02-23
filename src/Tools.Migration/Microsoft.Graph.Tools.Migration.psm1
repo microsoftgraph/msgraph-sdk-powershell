c5e192d609c773e599590033703cde2f85d4038b
Timothy Wamalwa <timwamalwa@gmail.com>
2023-02-24 01:45:07 +0300

    Scan scripts and write output to a table detailing the required changes

--- src/Tools.Migration/Microsoft.Graph.Tools.Migration.psm1
+++ src/Tools.Migration/Microsoft.Graph.Tools.Migration.psm1
@@ -1,4 +1,9 @@
-﻿function Read-MgScriptDirectory {
+﻿[System.Collections.ArrayList]$LineContent = @()
+[System.Collections.ArrayList]$LineNumbers = @()
+$LineUpdate = @{}
+$Original = @{}
+$ProposedChanges = @{}
+function Read-MgScriptDirectory {
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [ValidateNotNullorEmpty()]
@@ -9,7 +14,8 @@
         $supportedFileTypes = ".ps1", ".psm1"
         Get-ChildItem $DirectoryPath -Recurse -File | ForEach-Object {
             if ($_.Extension -in $supportedFileTypes) {
-                $scriptContent = Read-MgScript -FilePath $_.FullName
+                Read-MgScript -FilePath $_.FullName -GraphProfile $GraphProfile
+                
             }
             else {
                 Write-Warning "Skipping unsupported file type: $($_.FullName)"
@@ -22,7 +28,8 @@ function Read-MgScript {
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [ValidateNotNullorEmpty()]
-        [string] $FilePath
+        [string] $FilePath,
+        [string] $GraphProfile
     )
 
     process {
@@ -32,11 +39,21 @@ function Read-MgScript {
         }
 
         $scriptContent = Get-Content $FilePath
-        Write-Host "Read-MgScript: $FilePath :$($scriptContent.Length)"
+        #Write-Host "Read-MgScript: $FilePath :$($scriptContent.Length)"
 
-        $analysis = Invoke-MgScriptAnalyzer -ScriptContent $scriptContent
-
-        Write-Output $scriptContent
+        $Analysis = Invoke-MgScriptAnalyzer -ScriptContent $scriptContent -GraphProfile $GraphProfile
+        $Result = @()
+        foreach ($item in $Original.GetEnumerator() | Sort Name) {
+            $LineNumber = $item.Key
+            $OriginalValue = $Original[$LineNumber].ToString()
+            $ProposedValue = $ProposedChanges[$LineNumber].ToString()
+            $Result += [pscustomobject]@{"Location" = $FilePath; "Line number" = $LineNumber; "ApiVersion" = "Beta"; "Original" = $OriginalValue; "New" = $ProposedValue }
+            
+        }
+        if($Result.Count -gt 0){
+            Write-Host -ForegroundColor Green "--------- Command name updates on your script are required as per the details on the following table ---------"
+        }
+        Write-Output $Result | Format-Table
     }
 }
 
@@ -44,7 +61,8 @@ function Invoke-MgScriptAnalyzer {
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [ValidateNotNullorEmpty()]
-        [PSCustomObject] $ScriptContent
+        [PSCustomObject] $ScriptContent,
+        [string] $GraphProfile
     )
 
     # TODO: Analyze script content for breaking change.
@@ -54,6 +72,63 @@ function Invoke-MgScriptAnalyzer {
 
     # Proposed Output
     # Order | Location (file name:line:tab) | Type (Cmdlet|CmdletParameter|ModuleName) | ApiVersion | Original | New
+ 
+    $i = 0
+    foreach ($_ in $scriptContent) {
+        $LineContent.Add($_)
+
+        if ($_ -ieq "Select-MgProfile beta" -or $_ -ieq "Select-MgProfile 'beta'" -or $_ -ieq 'Select-MgProfile "beta"') {
+            
+            $LineNumbers.Add($i)
+        }
+    
+        $i++
+    }
+    if ($GraphProfile -ieq "beta") {
+        for ($g = 0; $g -lt $LineContent.Count; $g++) {
+            if ($LineContent[$g].Contains("Mg")) {
+                $Original.Add($g + 1, $LineContent[$g])
+                $ProposedChanges.Add($g + 1, $LineContent[$g].ToString().Replace("Mg", "MgBeta"))
+            }
+        }
+    }
+    else {
+        $Lines = $LineNumbers.Count
+
+        for ($j = 0; $j -lt $Lines; $j++) {
+         
+            for ($k = $LineNumbers[$j]; $k -lt $LineContent.Count; $k++) {
+                if ($LineContent[$k] -ieq "Select-MgProfile v1.0" -or $LineContent[$k] -ieq "Select-MgProfile 'v1.0'" -or $LineContent[$k] -ieq 'select-MgProfile "v1.0"') {
+            
+            
+                    if (-not $LineUpdate.ContainsKey($LineNumbers[$j])) {
+                        $LineUpdate.Add($LineNumbers[$j], $k)
+                    }
+                }
+            }
+        }
+        $LineUpdate.Keys | ForEach-Object {
+            $LineNumber = $_
+            for ($m = $lineNumber + 1; $m -lt $LineUpdate[$LineNumber]; $m++) {
+                if ($LineContent[$m].Contains("Mg")) {
+                    $Original.Add($m + 1, $LineContent[$m])
+                    $ProposedChanges.Add($m + 1, $LineContent[$m].ToString().Replace("Mg", "MgBeta"))
+                }
+            
+            }
+        }
+        if ($Lines -gt $LineUpdate.Count) {
+            for ($n = $LineNumbers[$Lines - 1] + 1; $n -lt $LineContent.Count; $n++) {
+                if ($LineContent[$n].Contains("Mg")) {
+                    $Original.Add($n + 1, $LineContent[$n])
+                    $ProposedChanges.Add($n + 1, $LineContent[$n].ToString().Replace("Mg", "MgBeta"))
+                }
+            }
+        }
+
+    }
+    
+
 }
 
 function New-MgMigrationPlan {
@@ -66,29 +141,30 @@ function New-MgMigrationPlan {
         [ValidateNotNullorEmpty()]
         [string] $FilePath,
         [Parameter(ParameterSetName = 'FileOrDirectory', Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
-        [PSCustomObject] $InputObject
+        [PSCustomObject] $InputObject,
+        [string] $GraphProfile
     )
 
     process {
         switch ($PSCmdlet.ParameterSetName) {
             'File' {
                 if (-not (Test-Path $FilePath)) { throw "Path does not exist: $FilePath" }
-                Read-MgScript -FilePath $FilePath
+                Read-MgScript -FilePath $FilePath -GraphProfile $GraphProfile
             }
             'Directory' {
                 if (-not (Test-Path $DirectoryPath)) { throw "Path does not exist: $DirectoryPath" }
-                Read-MgScriptDirectory -DirectoryPath $DirectoryPath
+                Read-MgScriptDirectory -DirectoryPath $DirectoryPath -GraphProfile $GraphProfile
             }
             'FileOrDirectory' {
                 foreach ($input in $InputObject) {
                     if (-not (Test-Path $input)) { throw "Path does not exist: $input" }
                     if ([System.IO.Path]::HasExtension($input)) {
                         # Process as file.
-                        Read-MgScript -FilePath $input
+                        Read-MgScript -FilePath $input -GraphProfile $GraphProfile
                     }
                     else {
                         # Process as directory.
-                        Read-MgScriptDirectory -DirectoryPath $input
+                        Read-MgScriptDirectory -DirectoryPath $input -GraphProfile $GraphProfile
                     }
                 }
             }
@@ -97,8 +173,8 @@ function New-MgMigrationPlan {
 }
 
 # TODO: Remove test when done.
-# New-MgMigrationPlan -FilePath "C:\Dev\M\msgraph-sdk-powershell\samples\6-Sites.ps1"
-
+#New-MgMigrationPlan -FilePath "C:\Dev\M\msgraph-sdk-powershell\samples\6-Sites.ps1"
+#New-MgMigrationPlan -FilePath "C:\Projects\msgraph-sdk-powershell\samples\6-Sites.ps1"
 # Test cases:
 # 1. Cmdlet with no changes.
 # 2. Cmdlet with changes.
