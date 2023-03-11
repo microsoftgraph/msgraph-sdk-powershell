e27e5119441c89717e2ad3f0419d6f8e3908fce3
Peter Ombwa <peter.ombwa@microsoft.com>
2023-03-10 16:52:32 -0800

    Use AssemblyLoadContextProxy in PS Core.

--- src/Authentication/Authentication/Utilities/DependencyAssemblyResolver.cs
+++ src/Authentication/Authentication/Utilities/DependencyAssemblyResolver.cs
@@ -2,6 +2,7 @@
 //  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 // ------------------------------------------------------------------------------
 
+using Microsoft.Graph.PowerShell.Authentication.Core.Utilities;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,51 +12,57 @@ namespace Microsoft.Graph.PowerShell.Authentication.Utilities
 {
     public static class DependencyAssemblyResolver
     {
+        private static readonly Assembly Self = typeof(DependencyAssemblyResolver).Assembly;
+        private static readonly Assembly Core = typeof(AuthenticationHelpers).Assembly;
+        private static readonly AssemblyLoadContextProxy Proxy = AssemblyLoadContextProxy.CreateLoadContext("msgraph-load-context");
+
         // Catalog our dependencies here to ensure we don't load anything else.
-        private static readonly IReadOnlyDictionary<string, Version> Dependencies = new Dictionary<string, Version>
+        private static readonly HashSet<string> Dependencies = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
         {
-            { "Azure.Core", new Version("1.28.0") },
-            { "Azure.Identity", new Version("1.8.2") },
-            { "Microsoft.Bcl.AsyncInterfaces", new Version("6.0.0") },
-            { "Microsoft.Graph.Core", new Version("2.0.15") },
-            { "Microsoft.Identity.Client", new Version("4.50.0") },
-            { "Microsoft.Identity.Client.Extensions.Msal", new Version("2.26.0") },
-            { "Microsoft.IdentityModel.Abstractions", new Version("6.27.0") },
-            { "Microsoft.IdentityModel.JsonWebTokens", new Version("6.27.0") },
-            { "Microsoft.IdentityModel.Logging", new Version("6.27.0") },
-            { "Microsoft.IdentityModel.Tokens", new Version("6.27.0") },
-            { "System.IdentityModel.Tokens.Jwt", new Version("6.27.0") },
-            { "System.Security.Cryptography.ProtectedData", new Version("7.0.1") },
-            { "Newtonsoft.Json", new Version("13.0.2") },
-            { "System.Text.Json", new Version("7.0.2") },
-            { "System.Text.Encodings.Web", new Version("6.0.0") },
-            { "System.Threading.Tasks.Extensions", new Version("4.5.4") },
-            { "System.Diagnostics.DiagnosticSource", new Version("4.0.4") },
-            { "System.Runtime.CompilerServices.Unsafe", new Version("4.0.4") },
-            { "System.Memory", new Version("4.0.1") },
-            { "System.Buffers", new Version("4.0.2") },
-            { "System.Numerics.Vectors", new Version("4.1.3") },
-            { "System.Net.Http.WinHttpHandler", new Version("6.0.0") }
+            "Azure.Core",
+            "Azure.Identity",
+            "Microsoft.Bcl.AsyncInterfaces",
+            "Microsoft.Graph.Core",
+            "Microsoft.Identity.Client",
+            "Microsoft.Identity.Client.Extensions.Msal",
+            "Microsoft.IdentityModel.Abstractions",
+            "Microsoft.IdentityModel.JsonWebTokens",
+            "Microsoft.IdentityModel.Logging",
+            "Microsoft.IdentityModel.Tokens",
+            "System.IdentityModel.Tokens.Jwt",
+            "System.Security.Cryptography.ProtectedData",
+            "Newtonsoft.Json",
+            "System.Text.Json",
+            "System.Text.Encodings.Web",
+            "System.Threading.Tasks.Extensions",
+            "System.Diagnostics.DiagnosticSource",
+            "System.Runtime.CompilerServices.Unsafe",
+            "System.Memory",
+            "System.Buffers",
+            "System.Numerics.Vectors",
+            "System.Net.Http.WinHttpHandler"
         };
 
         /// <summary>
         /// Dependencies that need to be loaded per framework.
         /// </summary>
         private static readonly IList<string> MultiFrameworkDependencies = new List<string> {
+            "Azure.Core",
+            "Azure.Identity",
             "Microsoft.Identity.Client",
+            "Microsoft.Identity.Client.Extensions.Msal",
             "System.Security.Cryptography.ProtectedData",
             "Microsoft.Graph.Core",
             "System.Net.Http.WinHttpHandler"
         };
 
         // Set up the path to our dependency directory within the module.
-        private static readonly string DependenciesDirPath = Path.GetFullPath(Path.Combine(
-                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Dependencies"));
+        private static readonly string DependencyFolder = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Self.Location), "Dependencies"));
 
         /// <summary>
         /// Framework dependency path. /Desktop for PS 5.1, and /Core for PS 6+.
         /// </summary>
-        private static string FrameworkDependenciesDirPath;
+        private static string PSEdition;
 
         /// <summary>
         /// Initializes our custom assembly resolve event handler. This should be called on module import.
@@ -63,16 +70,9 @@ namespace Microsoft.Graph.PowerShell.Authentication.Utilities
         /// <param name="isDesktopEdition"></param>
         public static void Initialize(bool isDesktopEdition = false)
         {
-            if (isDesktopEdition)
-            {
-                FrameworkDependenciesDirPath = "Desktop";
-            }
-            else
-            {
-                FrameworkDependenciesDirPath = "Core";
-            }
+            PSEdition = isDesktopEdition ? "Desktop" : "Core";
             // Set up our event handler when the module is loaded.
-            AppDomain.CurrentDomain.AssemblyResolve += HandleResolveEvent;
+            AppDomain.CurrentDomain.AssemblyResolve += ResolvingHandler;
         }
 
         /// <summary>
@@ -82,28 +82,29 @@ namespace Microsoft.Graph.PowerShell.Authentication.Utilities
         internal static void Reset()
         {
             // Remove our event hander when the module is unloaded.
-            AppDomain.CurrentDomain.AssemblyResolve -= HandleResolveEvent;
+            AppDomain.CurrentDomain.AssemblyResolve -= ResolvingHandler;
         }
 
-        private static Assembly HandleResolveEvent(object sender, ResolveEventArgs args)
+        private static Assembly ResolvingHandler(object sender, ResolveEventArgs args)
         {
             try
             {
-                AssemblyName assemblymName = new AssemblyName(args.Name);
+                AssemblyName assemblyName = new AssemblyName(args.Name);
                 // We try to resolve our dependencies on our own.
-                if (Dependencies.TryGetValue(assemblymName.Name, out Version requiredVersion)
-                    && (requiredVersion.Major >= assemblymName.Version.Major || string.Equals(assemblymName.Name, "Newtonsoft.Json", StringComparison.OrdinalIgnoreCase)))
+                if (Dependencies.Contains(assemblyName.Name))
                 {
-                    string requiredAssemblyPath = string.Empty;
-                    if (MultiFrameworkDependencies.Contains(assemblymName.Name))
+                    string requiredAssemblyPath = MultiFrameworkDependencies.Contains(assemblyName.Name)
+                        ? requiredAssemblyPath = Path.Combine(DependencyFolder, PSEdition, $"{assemblyName.Name}.dll")
+                        : requiredAssemblyPath = Path.Combine(DependencyFolder, $"{assemblyName.Name}.dll");
+                    if (File.Exists(requiredAssemblyPath))
                     {
-                        requiredAssemblyPath = Path.Combine(DependenciesDirPath, FrameworkDependenciesDirPath, $"{assemblymName.Name}.dll");
+                        // - In .NET, load the assembly into the custom assembly load context.
+                        // - In .NET Framework, assembly conflict is not a problem, so we load the assembly
+                        //   by 'Assembly.LoadFrom', the same as what powershell.exe would do.
+                        return Proxy != null
+                            ? Proxy.LoadFromAssemblyPath(requiredAssemblyPath)
+                            : Assembly.LoadFrom(requiredAssemblyPath);
                     }
-                    else
-                    {
-                        requiredAssemblyPath = Path.Combine(DependenciesDirPath, $"{assemblymName.Name}.dll");
-                    }
-                    return Assembly.LoadFile(requiredAssemblyPath);
                 }
             }
             catch
@@ -111,6 +112,37 @@ namespace Microsoft.Graph.PowerShell.Authentication.Utilities
                 // If an error is encountered, we fall back to PowerShell's default dependency resolution.
             }
             return null;
+        }
+    }
+
+    internal class AssemblyLoadContextProxy
+    {
+        private readonly object CustomContext;
+        private readonly MethodInfo loadFromAssemblyPathMethod;
+
+        private AssemblyLoadContextProxy(Type alc, string loadContextName)
+        {
+            var ctor = alc.GetConstructor(new[] { typeof(string), typeof(bool) });
+            loadFromAssemblyPathMethod = alc.GetMethod("LoadFromAssemblyPath", new[] { typeof(string) });
+            CustomContext = ctor.Invoke(new object[] { loadContextName, false });
+        }
+
+        internal Assembly LoadFromAssemblyPath(string assemblyPath)
+        {
+            return (Assembly)loadFromAssemblyPathMethod.Invoke(CustomContext, new[] { assemblyPath });
+        }
+
+        internal static AssemblyLoadContextProxy CreateLoadContext(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            var alc = typeof(object).Assembly.GetType("System.Runtime.Loader.AssemblyLoadContext");
+            return alc != null
+                ? new AssemblyLoadContextProxy(alc, name)
+                : null;
         }
     }
 }
