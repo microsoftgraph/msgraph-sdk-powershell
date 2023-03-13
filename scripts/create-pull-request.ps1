e703f54d166b610a6e218e3e6e2bc8db80dcb448
Peter Ombwa <peter.ombwa@microsoft.com>
2023-03-13 10:45:24 -0700

    Remove create PR GitHub actions.

---
+++ scripts/create-pull-request.ps1
@@ -0,0 +1,17 @@
+﻿# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+$title = $env:Title
+$body = $env:Body
+$baseBranchParameter = ""
+
+if (![string]::IsNullOrEmpty($env:BaseBranch))
+{
+    $baseBranchParameter = "-B $env:BaseBranch" # optionally pass the base branch if provided as the PR will target the default branch otherwise
+}
+
+# Code owners will be added automatically as reviewers.
+Invoke-Expression "gh auth login" # login to GitHub
+Invoke-Expression "gh pr create -t ""$title"" -b ""$body"" $baseBranchParameter | Write-Host"
+
+Write-Host "Pull Request Created successfully." -ForegroundColor Green
