0417dcd74f8b6c77e4319b3199010b963e6a5dc2
Peter Ombwa <peombwa@microsoft.com>
2023-04-05 08:33:31 -0700

    Update v2 build pipeline (#1919)
    * Change devX API endpoint for v2.
    
    * Create PRs in AzDo.
    
    * Adds steps to create-pr template.
    
    * Adds SkipOpenAPIDocsDownload condition.
    
    * Skip build when ModulesWithChanges is empty.
    
    * Handle SkipOpenAPIDocsDownload as string.
    
    * Add head branch
    
    * Bump versions of credscan and policheck
    
    * Update post build security checks
    
    * Use .NET 6 for ESRP signing
    
    * Use default code sign options.
    
    * Suppress examples folder for cred scan
    
    * Add cred scan suppressions file
    
    * Remove v1 SDK modules
    
    * Add DeviceManagement.Actions examples to credscan suppression file
    
    * Disable PoliCheck
    
    * Add PoliCheck exclusions.
    
    * Exclude generated examples from poli check
    
    * Set path to policheck exclusion
    
    * Move compliance task config to scripts folder
    
    * Use fully qualified paths
    
    * Build all modules

--- tools/DownloadOpenApiDoc.ps1
+++ tools/DownloadOpenApiDoc.ps1
@@ -12,8 +12,8 @@ if (-not (Test-Path $OpenApiDocOutput)) {
     New-Item -Path $OpenApiDocOutput -Type Directory
 }
 
-$OpenApiBaseUrl = "https://devxapitest.azurewebsites.net"
-$OpenApiServiceUrl = ("$OpenApiBaseUrl/`$openapi?tags={0}&title=$ModuleName&openapiversion=3&style=Powershell&graphVersion=$GraphVersion" -f $ModuleRegex)
+$OpenApiBaseUrl = "https://graphexplorerapi.azurewebsites.net"
+$OpenApiServiceUrl = ("$OpenApiBaseUrl/`$openapi?tags={0}&title=$ModuleName&openapiversion=3&style=Powershell&fileName=powershell_v2&graphVersion=$GraphVersion" -f $ModuleRegex)
 if ($ForceRefresh.IsPresent) {
     $OpenApiServiceUrl = "$OpenApiServiceUrl&forceRefresh=true"
 }
