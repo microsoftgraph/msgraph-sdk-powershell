759aaf3bde60caefa3e7a555fd372baaaffa8fc8
Timothy Wamalwa <timwamalwa@gmail.com>
2023-05-16 12:54:49 +0300

    Made changes to git config

--- tools/ExamplesGenerator.ps1
+++ tools/ExamplesGenerator.ps1
@@ -43,6 +43,8 @@ function Start-Generator {
         Get-ExternalDocsUrl -ManualExternalDocsUrl $ManualExternalDocsUrl -GenerationMode $GenerationMode -GraphProfilePath $ModulePath -Command $GraphCommand -GraphProfile $ProfilePath -Module $GraphModule
             
     }
+    git add .
+    git commit -m "Importing updates from API reference" 
 }
 function Get-FilesByProfile {
     Param(
@@ -404,7 +406,7 @@ function Update-ExampleFile {
     }
   
 }
-        
+Set-Location msgraph-sdk-powershell       
 $JsonContent = Get-Content -Path $MetaDataJsonFile
 $DeserializedContent = $JsonContent | ConvertFrom-Json
 foreach($Data in $DeserializedContent)
@@ -445,7 +447,7 @@ if ($ModulesToGenerate.Count -eq 0) {
     [HashTable] $ModuleMapping = Get-Content $ModuleMappingConfigPath | ConvertFrom-Json -AsHashTable
     $ModulesToGenerate = $ModuleMapping.Keys
 }
-
+Set-Location ..\msgraph-sdk-powershell
 Start-Generator -ModulesToGenerate $ModulesToGenerate -GenerationMode "auto"
 
 #Comment the above and uncomment the below start command, if you manually want to manually pass ExternalDocs url.
