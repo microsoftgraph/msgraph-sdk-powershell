e1351978f02bc425c2bd75e0590e68a90b85eaef
Timothy Wamalwa <timwamalwa@gmail.com>
2023-09-21 13:21:59 +0300

    Added function for updating metadata on the list

--- src/Authentication/Authentication/import-synopsis-and-descriptions.ps1
+++ src/Authentication/Authentication/import-synopsis-and-descriptions.ps1
@@ -1,13 +1,19 @@
-[CmdletBinding()]
 Param(
-  [string] $DocsLocation = (Join-Path $PSScriptRoot "../docs/"),
-  [string] $SynopsisLocation = (Join-Path $PSScriptRoot "../synopsis/"),
-  [string] $DescriptionLocation = (Join-Path $PSScriptRoot "../descriptions/")
+    [string] $DocsLocation = (Join-Path $PSScriptRoot "../docs/"),
+    [string] $CmdletsList = (Join-Path $PSScriptRoot "../docs/Microsoft.Graph.Authentication.md"),
+    [string] $SynopsisLocation = (Join-Path $PSScriptRoot "../synopsis/"),
+    [string] $DescriptionLocation = (Join-Path $PSScriptRoot "../descriptions/")
   
 )
 $ImportMapping = @{
     "descriptions" = "## DESCRIPTION(?s).*## EXAMPLES"
-    "synopsis" = "## SYNOPSIS(?s).*## SYNTAX"
+    "synopsis"     = "## SYNOPSIS(?s).*## SYNTAX"
+}
+$MetaDataMapping = @{
+    "Module Guid:"        = "Module Guid:.+"
+    "Download Help Link:" = "Download Help Link:.+"
+    "Locale:"             = "Locale:.+"
+    "Help Version:"       = "Help Version:.+"
 }
 function Start-Import {
     foreach ($File in Get-ChildItem $DocsLocation) {
@@ -15,24 +21,29 @@ function Start-Import {
             $ImportContent = $_
             $SearchBlock = $ImportMapping[$ImportContent]
             $LocationFile = (Join-Path $SynopsisLocation $File.Name)
-            if($ImportContent -eq "descriptions") {
+            if ($ImportContent -eq "descriptions") {
                 $LocationFile = (Join-Path $DescriptionLocation $File.Name)
             }
 
     
-        if (Test-Path $LocationFile) {
-           $FileEmpty = Test-FileEmpty -File $LocationFile
-           if(-not($FileEmpty)) {
-              Write-Host $LocationFile
-              $Content = Get-Content -Path $LocationFile
-              $FinalOutput = "## SYNOPSIS`r`n$Content`r`n## SYNTAX"
-              if($ImportContent -eq "descriptions") {
-                $FinalOutput = "## DESCRIPTION`r`n$Content`r`n## EXAMPLES"
-              }
-              Import-Content -Content $Content -File $File -SearchBlock $SearchBlock -FinalOutput $FinalOutput
-           }
+            if (Test-Path $LocationFile) {
+                $FileEmpty = Test-FileEmpty -File $LocationFile
+                if (-not($FileEmpty)) {
+                    Write-Host $LocationFile
+                    $Content = Get-Content -Path $LocationFile
+                    $FinalOutput = "## SYNOPSIS`r`n$Content`r`n## SYNTAX"
+                    if ($ImportContent -eq "descriptions") {
+                        $FinalOutput = "## DESCRIPTION`r`n$Content`r`n## EXAMPLES"
+                    }
+                    Import-Content -Content $Content -File $File -SearchBlock $SearchBlock -FinalOutput $FinalOutput
+                }
+            }
         }
     }
+    $MetaDataMapping.Keys | ForEach-Object {
+        $MetaDataContent = $_
+        $SearchBlock = $MetaDataMapping[$MetaDataContent]
+        UpdateMetaDataHeader -SearchBlock $SearchBlock -MetaDataContent $MetaDataContent
     }
 }
 
@@ -47,33 +58,62 @@ function Import-Content {
 
     $option = [System.Text.RegularExpressions.RegexOptions]::Multiline
     $Re = [regex]::new($SearchBlock, $option)
-    if(Test-Path $File){
+    if (Test-Path $File) {
         $DestinationContent = Get-Content -Encoding UTF8 -Raw $File
-     if($DestinationContent -match $Re){
-        $Extracted = $Matches[0]
-        $text = $DestinationContent.ToString()
-        $text = $text.Replace($Extracted, $FinalOutput)
+        if ($DestinationContent -match $Re) {
+            $Extracted = $Matches[0]
+            $text = $DestinationContent.ToString()
+            $text = $text.Replace($Extracted, $FinalOutput)
         }
-          $text | Out-File $File -Encoding UTF8
-     }
-     $Stream = [IO.File]::OpenWrite($File)
-     try
-     {
-         $Stream.SetLength($stream.Length - 2)
-         $Stream.Close()
-     }
-     catch
-     {
-         
-     }
-     $Stream.Dispose()
+        $text | Out-File $File -Encoding UTF8
     }
+    $Stream = [IO.File]::OpenWrite($File)
+    try {
+        $Stream.SetLength($stream.Length - 2)
+        $Stream.Close()
+    }
+    catch {
+         
+    }
+    $Stream.Dispose()
+}
+function UpdateMetaDataHeader {
+    Param (
+        [string]$SearchBlock,
+        [string]$MetaDataContent
+    )
+    $option = [System.Text.RegularExpressions.RegexOptions]::Multiline
+    $Re = [regex]::new($SearchBlock, $option)
+    $DestinationContent = Get-Content -Encoding UTF8 -Raw $CmdletsList
+    $text = $DestinationContent.ToString()
+    if ($DestinationContent -match $Re) {
+        $Extracted = $Matches[0]
+        if ($MetaDataContent -eq "Module Guid:") {
+            $Guid = "Module Guid: " + [guid]::NewGuid()
+            $text = $text.Replace($Extracted, $Guid) 
+        }
+        elseif ($MetaDataContent -eq "Download Help Link:") {
+            $Link = "Download Help Link: https://learn.microsoft.com/powershell/module/Microsoft.Graph.Authentication"
+            $text = $text.Replace($Extracted, $Link) 
+        }
+        elseif ($MetaDataContent -eq "Locale:") {
+            $Locale = "Locale: en-US"
+            $text = $text.Replace($Extracted, $Locale) 
+        }
+        elseif ($MetaDataContent -eq "Help Version:") {
+            $HelpVersion = "Help Version: 1.0.0.0"
+            $text = $text.Replace($Extracted, $HelpVersion) 
+        }
+        $text = $text.Replace("{{ Fill in the Description }}", "Microsoft Graph PowerShell Authentication Cmdlets")
+        $text | Out-File $CmdletsList -Encoding UTF8
+    }
+}
 
 Function Test-FileEmpty {
 
     Param ([Parameter(Mandatory = $true)][string]$File)
   
-    if ((Test-Path -LiteralPath $File) -and !((Get-Content -LiteralPath $file -Raw) -match '\S')) {return $true} else {return $false}
+    if ((Test-Path -LiteralPath $File) -and !((Get-Content -LiteralPath $file -Raw) -match '\S')) { return $true } else { return $false }
   
-  }
+}
 Start-Import
