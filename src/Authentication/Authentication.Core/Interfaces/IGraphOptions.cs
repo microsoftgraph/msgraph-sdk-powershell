2cc80e8641b470128f4613292ee63367073be850
Taofeek F. Obafemi-Babatunde <taofeeko@microsoft.com>
2023-12-28 10:52:21 -0800

    adding "-AT PoP" option to "Set-MgGraphOptions"

--- src/Authentication/Authentication.Core/Interfaces/IGraphOptions.cs
+++ src/Authentication/Authentication.Core/Interfaces/IGraphOptions.cs
@@ -11,5 +11,6 @@ namespace Microsoft.Graph.PowerShell.Authentication
     public interface IGraphOption
     {
         bool EnableWAMForMSGraph { get; set; }
+        bool EnableATPoPForMSGraph { get; set; }
     }
 }
