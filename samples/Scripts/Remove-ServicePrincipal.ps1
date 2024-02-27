818543b21d7e9dfc14930934bba213305766a603
ElinorW <elkaguon@microsoft.com>
2024-02-27 16:14:21 +0300

    add new app id

--- samples/Scripts/Remove-ServicePrincipal.ps1
+++ samples/Scripts/Remove-ServicePrincipal.ps1
@@ -1,5 +1,5 @@
 # Removes Microsoft Graph PowerShell (preview) Service Principal
 # This will cause you to need to reconsent after doing Disconnect-Graph
 
-$sp = Get-MgServicePrincipal -top 999 | ? { $_.AppId -like "14d82eec-204b-4c2f-b7e8-296a70dab67e" }
+$sp = Get-MgServicePrincipal -top 999 | ? { $_.AppId -like "3849bfaa-deab-480f-a8b1-39dc5bf58c35" }
 Remove-MgServicePrincipal -ServicePrincipalId $sp.Id 
