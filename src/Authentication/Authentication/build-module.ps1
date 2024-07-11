63beaaec21b119494dfaa0dcbd97e3183ea88ae9
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2024-07-11 12:05:20 +0300

    Adds missing descriptions during cmdlet generation

--- src/Authentication/Authentication/build-module.ps1
+++ src/Authentication/Authentication/build-module.ps1
@@ -108,6 +108,49 @@ Write-Host -ForegroundColor Green 'Creating new xml based help file ...'
 $DocsPath = (Join-Path $PSScriptRoot "../docs/")
 New-ExternalHelp -Path $DocsPath -OutputPath $PSScriptRoot -Force
 
+# Update markdown xml by replacing place holders with actual descriptions for script based cmdlets.
+
+# Place holder values for Find-MgGraphPermission
+$SearchStringDescription = "The SearchString parameter allows you to specify a string such as 'user' or 'mail' that represents the subject or domain
+of the permission you're searching for. Since permissions usually have names such as 'User.Read' or 'Mail.ReadWrite', the
+command uses the SearchString parameter to return all permissions that contain the value specified for SearchString in the
+name of the permission."
+
+$ExactMatchDescription = "Specify the ExactMatch parameter to restrict the permissions emitted to those that exactly match the value specified for SearchString."
+
+$PermissionTypeDescription = "Specify the PermissionType to determine whether application permissions, delegated permisisons, or both are returned by
+Find-MgGraphPermission. By default, the value of this parameter is Any, which includes both delegated and application permissions.
+Other valid values for PermissionType are Application and Delegated to return those specify types of permissions."
+
+$OnlineDescription = "Specify the Online parameter in addition to SearchString to force Find-MgGraphPermission to update its set of permissions
+by requesting the latest permissions data from Microsoft Graph itself before searching for the permissions specified the
+SearchString parameter. This ensures that Find-MgGraphPermission returns the most accurate search results as new permissions
+are added to Microsoft Graph for new APIs. The command uses the existing access to Microsoft Graph as enabled by a previous
+invocation of the Connect-MgGraph command to issue the request for updated permissions. If your current connection does not
+already have access to read this data from Microsoft Graph or if there is no network connectivity to Microsoft Graph, the command will fail.
+If the command is successful in updating the set of permissions prior to searching for permissions, Find-MgGraphPermission will
+continue to use the updated list for all future invocations of the command even if they do not specify the Online parameter"
+
+$AllParameterDescription = "To return all possible permissions rather than just those that match the SearchString parameter, specify the All parameter. The
+All parameter may also be used with the PermissionType to enumerate all applicaition permissions or all delegated permissions."
+
+# Place holder values for Find-MgGraphCommand
+$UriDescription = "The API path a command calls. e.g., /users."
+
+$MethodDescription = "The HTTP method a command makes."
+
+$ApiVersionDescription = "The service API version."
+
+$CommandDescription = "The name of a command. e.g., Get-MgUser."
+
+# Update maml file by replacing place holders with actual value.
+(Get-Content "$cmdletsSrc/$ModulePrefix.$ModuleName-Help.xml") -replace "{{ Fill ApiVersion Description }}", $ApiVersionDescription -replace "{{ Fill Command Description }", $CommandDescription -replace "{{ Fill InputObject Description }}", "Pipeline input object" -replace "{{ Fill Uri Description }}", $UriDescription -replace "{{ Fill Method Description }}", $MethodDescription -replace "{{ Fill Command Description }}", $CommandDescription -replace "{{ Fill All Description }}", $AllParameterDescription -replace "{{ Fill Online Description }}", $OnlineDescription -replace "{{ Fill PermissionType Description }}", $PermissionTypeDescription -replace "{{ Fill SearchString Description }}", $SearchStringDescription -replace "{{ Fill ExactMatch Description }}", $ExactMatchDescription -replace "{{ Fill ProgressAction Description }}", "Treat this as a common parameter." | Set-Content "$cmdletsSrc/$ModulePrefix.$ModuleName-Help.xml"
+
+# Update Find-MgGraphPermission markdown file by replacing place holders with actual value.
+(Get-Content "$DocsPath/Find-MgGraphPermission.md") -replace "{{ Fill All Description }}", $AllParameterDescription -replace "{{ Fill Online Description }}", $OnlineDescription -replace "{{ Fill PermissionType Description }}", $PermissionTypeDescription -replace "{{ Fill SearchString Description }}", $SearchStringDescription -replace "{{ Fill ExactMatch Description }}", $ExactMatchDescription -replace "{{ Fill ProgressAction Description }}", "Treat this as a common parameter." | Set-Content "$DocsPath/Find-MgGraphPermission.md"
+
+# Update Find-MgGraphCommand markdown file by replacing place holders with actual value.
+(Get-Content "$DocsPath/Find-MgGraphCommand.md") -replace "{{ Fill ApiVersion Description }}", $ApiVersionDescription -replace "{{ Fill Command Description }", $CommandDescription -replace "{{ Fill InputObject Description }}", "Pipeline input object" -replace "{{ Fill Uri Description }}", $UriDescription -replace "{{ Fill Method Description }}", $MethodDescription -replace "{{ Fill Command Description }}", $CommandDescription -replace "{{ Fill ProgressAction Description }}", "Treat this as a common parameter."| Set-Content "$DocsPath/Find-MgGraphCommand.md"
 # Copy manifest.
 Copy-Item -Path "$cmdletsSrc/$ModulePrefix.$ModuleName.format.ps1xml" -Destination $outDir
 Copy-Item -Path "$cmdletsSrc/$ModulePrefix.$ModuleName.psm1" -Destination $outDir
