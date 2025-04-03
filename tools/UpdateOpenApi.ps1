1e346df8b83b3ade7c4840bcd975acb08afa0a88
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2025-04-03 21:38:50 +0300

    Added skip for Devices.Corporatemanagement

--- tools/UpdateOpenApi.ps1
+++ tools/UpdateOpenApi.ps1
@@ -46,8 +46,11 @@ $ModuleMapping.Keys | ForEach-Object -Begin { $RequestCount = 0 } -End { Write-D
         }
 
         try {
-            # Download OpenAPI document for module.
-            & $DownloadOpenApiDocPS1 -ModuleName $ModuleName -ModuleRegex $ModuleMapping[$ModuleName] -OpenApiDocOutput $OpenApiDocOutput -GraphVersion $GraphVersion -ForceRefresh:$ForceRefresh -RequestCount $RequestCount -SingularizeOperationIds
+            #Skip Devices.Corporatemanagement module for further troubleshooting.
+            if (-not($ModuleName -eq "Devices.CorporateManagement")) {
+                # Download OpenAPI document for module.
+                & $DownloadOpenApiDocPS1 -ModuleName $ModuleName -ModuleRegex $ModuleMapping[$ModuleName] -OpenApiDocOutput $OpenApiDocOutput -GraphVersion $GraphVersion -ForceRefresh:$ForceRefresh -RequestCount $RequestCount -SingularizeOperationIds
+            }
         }
         catch {
             Write-Error $_.Exception
