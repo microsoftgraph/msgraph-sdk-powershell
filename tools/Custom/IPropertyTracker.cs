e7e4e5f02011d6eb61f89035845da8031c1e4fcd
Timothy Wamalwa <timwamalwa@gmail.com>
2025-05-08 10:39:41 +0300

    Removed faulty property tracking related files

--- tools/Custom/IPropertyTracker.cs
+++
@@ -1,9 +0,0 @@
-namespace NamespacePrefixPlaceholder.PowerShell.Models
-{
-    public interface IPropertyTracker
-    {
-        void TrackProperty(string propertyName);
-        bool IsPropertySet(string propertyName);
-        T SanitizeValue<T>(object value);
-    }
-}
